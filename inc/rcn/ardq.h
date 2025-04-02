c38040612314d0f8a55d937a87a967ff2b8fd754
YOUNGJIN JOO <neoelec@gmail.com>
2025-04-02 21:41:33 +0900

    ardq: introduce a new container. Array Deque
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn/ardq.h
@@ -0,0 +1,97 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Array Deque */
+#ifndef __ARDQ_H__
+#define __ARDQ_H__
+
+#include "common.h"
+
+struct ardq {
+    size_t nr_entries;
+    size_t count;
+    size_t front;
+    size_t rear;
+    void *pool[];
+};
+
+#define ARDQ_BYTES(nr_entries) \
+    (sizeof(struct ardq) + (nr_entries) * (sizeof(void *)))
+
+#define ARDQ_MIN_NR_ENTRIES 8
+
+static inline void ardq_initialize(struct ardq *deque, size_t nr_entries)
+{
+    assert(nr_entries >= ARDQ_MIN_NR_ENTRIES);
+
+    deque->nr_entries = nr_entries;
+    deque->count = 0;
+    deque->front = deque->nr_entries - 1;
+    deque->rear = 0;
+}
+
+static inline size_t ardq_count(const struct ardq *deque)
+{
+    return deque->count;
+}
+
+static inline bool ardq_is_empty(const struct ardq *deque)
+{
+    return deque->count == 0;
+}
+
+static inline bool ardq_is_full(const struct ardq *deque)
+{
+    return deque->count == deque->nr_entries;
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int ardq_push_front(struct ardq *deque, void *e);
+extern int ardq_push_rear(struct ardq *deque, void *e);
+extern int ardq_pop_front(struct ardq *deque, void **_e);
+extern int ardq_pop_rear(struct ardq *deque, void **_e);
+extern int ardq_front(const struct ardq *deque, void **_e);
+extern int ardq_rear(const struct ardq *deque, void **_e);
+extern void ardq_forward(struct ardq *deque, do_each_t do_each, void *priv);
+extern void ardq_backward(struct ardq *deque, do_each_t do_each, void *priv);
+
+#ifdef __cplusplus
+}
+#endif
+
+static inline int ardq_push_front_i(struct ardq *deque, intptr_t v)
+{
+    return ardq_push_front(deque, (void *)v);
+}
+
+static inline int ardq_push_rear_i(struct ardq *deque, intptr_t v)
+{
+    return ardq_push_rear(deque, (void *)v);
+}
+
+static inline int ardq_pop_front_i(struct ardq *deque, intptr_t *_v)
+{
+    return ardq_pop_front(deque, (void **)_v);
+}
+
+static inline int ardq_pop_rear_i(struct ardq *deque, intptr_t *_v)
+{
+    return ardq_pop_rear(deque, (void **)_v);
+}
+
+static inline int ardq_front_i(const struct ardq *deque, intptr_t *_v)
+{
+    return ardq_front(deque, (void **)_v);
+}
+
+static inline int ardq_rear_i(const struct ardq *deque, intptr_t *_v)
+{
+    return ardq_rear(deque, (void **)_v);
+}
+
+#endif /* __ARDQ_H__ */
---
+++ src/ardq.c
@@ -0,0 +1,125 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+#include <errno.h>
+
+#include <rcn/ardq.h>
+
+int ardq_push_front(struct ardq *deque, void *e)
+{
+    if (ardq_is_full(deque))
+        return -ERANGE;
+
+    deque->count++;
+    if (++deque->front == deque->nr_entries)
+        deque->front = 0;
+
+    deque->pool[deque->front] = e;
+
+    return 0;
+}
+
+int ardq_push_rear(struct ardq *deque, void *e)
+{
+    if (ardq_is_full(deque))
+        return -ERANGE;
+
+    deque->count++;
+    deque->rear = deque->rear == 0 ? deque->nr_entries - 1 : deque->rear - 1;
+
+    deque->pool[deque->rear] = e;
+
+    return 0;
+}
+
+int ardq_pop_front(struct ardq *deque, void **_e)
+{
+    if (ardq_is_empty(deque))
+        return -ENODATA;
+
+    if (_e != NULL)
+        *_e = deque->pool[deque->front];
+
+    deque->count--;
+    deque->front = deque->front == 0 ? deque->nr_entries - 1 : deque->front - 1;
+
+    return 0;
+}
+
+int ardq_pop_rear(struct ardq *deque, void **_e)
+{
+    if (ardq_is_empty(deque))
+        return -ENODATA;
+
+    if (_e != NULL)
+        *_e = deque->pool[deque->rear];
+
+    deque->count--;
+    if (++deque->rear == deque->nr_entries)
+        deque->rear = 0;
+
+    return 0;
+}
+
+int ardq_front(const struct ardq *deque, void **_e)
+{
+    if (ardq_is_empty(deque))
+        return -ENODATA;
+
+    if (_e != NULL)
+        *_e = deque->pool[deque->front];
+
+    return 0;
+}
+
+int ardq_rear(const struct ardq *deque, void **_e)
+{
+    if (ardq_is_empty(deque))
+        return -ENODATA;
+
+    if (_e != NULL)
+        *_e = deque->pool[deque->rear];
+
+    return 0;
+}
+
+void ardq_forward(struct ardq *deque, do_each_t do_each, void *priv)
+{
+    ssize_t i, n, f, m, r;
+
+    if (ardq_is_empty(deque))
+        return;
+
+    n = ardq_count(deque);
+    f = (ssize_t)deque->front;
+    m = (ssize_t)deque->nr_entries - 1;
+    r = (ssize_t)deque->rear;
+
+    for (i = f; i >= 0 && n > 0; i--, n--)
+        do_each(deque->pool[i], priv);
+
+    for (i = m; i >= r && n > 0; i--, n--)
+        do_each(deque->pool[i], priv);
+}
+
+void ardq_backward(struct ardq *deque, do_each_t do_each, void *priv)
+{
+    size_t i, f, m, r;
+    ssize_t n;
+
+    if (ardq_is_empty(deque))
+        return;
+
+    n = ardq_count(deque);
+    f = deque->front;
+    m = deque->nr_entries - 1;
+    r = deque->rear;
+
+    for (i = r; i <= m && n > 0; i++, n--)
+        do_each(deque->pool[i], priv);
+
+    for (i = 0; i <= f && n > 0; i++, n--)
+        do_each(deque->pool[i], priv);
+}
