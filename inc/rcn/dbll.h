66f5f2140f051c2a2cc4ee0ba606ea65e88a1bec
YOUNGJIN JOO <neoelec@gmail.com>
2025-04-03 22:46:01 +0900

    dbll: introduce dbll container
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- inc/rcn/dbll.h
+++ inc/rcn/dbll.h
@@ -20,72 +20,107 @@ struct dbll_node {
     struct dbll_node *next;
 };
 
-static inline void dbll_initialize(struct dbll_node *list)
+static inline void dbll_node_initialize(struct dbll_node *x)
 {
-    list->prev = list;
-    list->next = list;
+    x->prev = x;
+    x->next = x;
 }
 
-static inline bool dbll_is_empty(const struct dbll_node *list)
+static inline bool dbll_node_is_orphaned(const struct dbll_node *x)
 {
-    return list->next == list && list->prev == list;
+    return x->next == x && x->prev == x;
 }
 
-static inline void dbll_link_next(struct dbll_node *p, struct dbll_node *x)
+static inline void dbll_node_link_next(struct dbll_node *x, struct dbll_node *y)
 {
-    p->next->prev = x;
-    x->next = p->next;
-    p->next = x;
-    x->prev = p;
+    struct dbll_node *z = x->next;
+
+    /* x<->z => x<->y<->z */
+    x->next = y;
+    y->prev = x;
+    z->prev = y;
+    y->next = z;
 }
 
-static inline void dbll_link_previous(struct dbll_node *p, struct dbll_node *x)
+static inline void dbll_node_link_prev(struct dbll_node *z, struct dbll_node *y)
 {
-    p->prev->next = x;
-    x->prev = p->prev;
-    p->prev = x;
-    x->next = p;
+    struct dbll_node *x = z->prev;
+
+    /* x<->z => x<->y<->z */
+    x->next = y;
+    y->prev = x;
+    z->prev = y;
+    y->next = z;
 }
 
-static inline void dbll_unlink(struct dbll_node *z)
+static inline void dbll_node_unlink(struct dbll_node *y)
 {
-    z->prev->next = z->next;
-    z->next->prev = z->prev;
-    z->prev = NULL;
-    z->next = NULL;
+    struct dbll_node *x = y->prev;
+    struct dbll_node *z = y->next;
+
+    /* x<->y<->z => x<->z */
+    x->next = z;
+    z->prev = x;
 }
 
-static inline void dbll_insert_head(struct dbll_node *list, struct dbll_node *x)
+struct dbll {
+    size_t count;
+    struct dbll_node pool;
+};
+
+static inline void dbll_initialize(struct dbll *list)
 {
-    dbll_link_next(list, x);
+    dbll_node_initialize(&list->pool);
+    list->count = 0;
 }
 
-static inline void dbll_insert_tail(struct dbll_node *list, struct dbll_node *x)
+static inline bool dbll_is_empty(const struct dbll *list)
 {
-    dbll_link_previous(list, x);
+    return list->count == 0;
+}
+
+static inline size_t dbll_count(const struct dbll *list)
+{
+    return list->count;
+}
+
+static inline void dbll_insert_head(struct dbll *list, struct dbll_node *x)
+{
+    dbll_node_link_next(&list->pool, x);
+    list->count++;
+}
+
+static inline void dbll_insert_tail(struct dbll *list, struct dbll_node *x)
+{
+    dbll_node_link_prev(&list->pool, x);
+    list->count++;
 }
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern size_t dbll_count(const struct dbll_node *list);
-extern int dbll_insert(struct dbll_node *list, size_t n, struct dbll_node *x);
-extern int dbll_remvoe(struct dbll_node *list, size_t n, struct dbll_node **_x);
-extern int dbll_remove_head(struct dbll_node *list, struct dbll_node **_x);
-extern int dbll_remove_tail(struct dbll_node *list, struct dbll_node **_x);
-extern int dbll_get(const struct dbll_node *list, size_t n,
-                    struct dbll_node **_x);
-extern int dbll_get_head(const struct dbll_node *list, struct dbll_node **_x);
-extern int dbll_get_tail(const struct dbll_node *list, struct dbll_node **_x);
+extern int dbll_node_search(const struct dbll_node *pool, dbll_compar_t compar,
+                            const struct dbll_node *kx, struct dbll_node **_x);
+extern void dbll_node_forward(struct dbll_node *pool, dbll_do_each_t do_each,
+                              void *priv);
+extern void dbll_node_backward(struct dbll_node *pool, dbll_do_each_t do_each,
+                               void *priv);
+extern void dbll_node_reverse(struct dbll_node *pool);
 
-extern int dbll_search(const struct dbll_node *list, dbll_compar_t compar,
+extern int dbll_insert(struct dbll *list, size_t n, struct dbll_node *x);
+extern int dbll_remvoe(struct dbll *list, size_t n, struct dbll_node **_x);
+extern int dbll_remove_head(struct dbll *list, struct dbll_node **_x);
+extern int dbll_remove_tail(struct dbll *list, struct dbll_node **_x);
+extern int dbll_get(const struct dbll *list, size_t n, struct dbll_node **_x);
+extern int dbll_get_head(const struct dbll *list, struct dbll_node **_x);
+extern int dbll_get_tail(const struct dbll *list, struct dbll_node **_x);
+extern int dbll_search(const struct dbll *list, dbll_compar_t compar,
                        const struct dbll_node *kx, struct dbll_node **_x);
-extern void dbll_forward(struct dbll_node *list, dbll_do_each_t do_each,
-                         void *priv);
-extern void dbll_backward(struct dbll_node *list, dbll_do_each_t do_each,
+extern void dbll_forward(struct dbll *list, dbll_do_each_t do_each, void *priv);
+extern void dbll_backward(struct dbll *list, dbll_do_each_t do_each,
                           void *priv);
-extern void dbll_reverse(struct dbll_node *list);
+extern void dbll_reverse(struct dbll *list);
 
 #ifdef __cplusplus
 }
--- src/dbll.c
+++ src/dbll.c
@@ -7,164 +7,12 @@
 
 #include <rcn/dbll.h>
 
-static int __head(const struct dbll_node *list, struct dbll_node **_x)
-{
-    *_x = list->next;
-
-    return dbll_is_empty(list) ? -ENODATA : 0;
-}
-
-static int __tail(const struct dbll_node *list, struct dbll_node **_x)
-{
-    *_x = list->prev;
-
-    return dbll_is_empty(list) ? -ENODATA : 0;
-}
-
-static int __get(const struct dbll_node *list, ssize_t n, struct dbll_node **_x,
-                 struct dbll_node **_p)
-{
-    struct dbll_node *x = list->next;
-    struct dbll_node *p = (struct dbll_node *)list;
-
-    if (dbll_is_empty(list))
-        return -ENODATA;
-
-    while (n-- > 0 && x != list) {
-        p = x;
-        x = x->next;
-    }
-
-    if (x == list)
-        return -ERANGE;
-
-    *_x = x;
-    *_p = p;
-
-    return 0;
-}
-
-size_t dbll_count(const struct dbll_node *list)
-{
-    const struct dbll_node *x;
-    size_t count = 0;
-
-    for (x = list->next; x != NULL; x = x->next)
-        count++;
-
-    return count;
-}
-
-int dbll_insert(struct dbll_node *list, size_t n, struct dbll_node *x)
-{
-    struct dbll_node *y, *p;
-    int err;
-
-    err = __get(list, n, &y, &p);
-    if (err != 0)
-        return err;
-
-    dbll_link_next(p, x);
-
-    return 0;
-}
-
-int dbll_remvoe(struct dbll_node *list, size_t n, struct dbll_node **_x)
-{
-    struct dbll_node *x, *p;
-    int err;
-
-    err = __get(list, n, &x, &p);
-    if (err != 0)
-        return err;
-
-    dbll_unlink(x);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return 0;
-}
-
-int dbll_remove_head(struct dbll_node *list, struct dbll_node **_x)
-{
-    struct dbll_node *x;
-    int err;
-
-    err = __head(list, &x);
-    if (err != 0)
-        return err;
-
-    dbll_unlink(list->next);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return 0;
-}
-
-int dbll_remove_tail(struct dbll_node *list, struct dbll_node **_x)
-{
-    struct dbll_node *x;
-    int err;
-
-    err = __tail(list, &x);
-    if (err != 0)
-        return err;
-
-    dbll_unlink(list->next);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return 0;
-}
-
-int dbll_get(const struct dbll_node *list, size_t n, struct dbll_node **_x)
-{
-    struct dbll_node *x, *p;
-    int err;
-
-    err = __get(list, n, &x, &p);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int dbll_get_head(const struct dbll_node *list, struct dbll_node **_x)
-{
-    struct dbll_node *x;
-    int err;
-
-    err = __head(list, &x);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int dbll_get_tail(const struct dbll_node *list, struct dbll_node **_x)
-{
-    struct dbll_node *x;
-    int err;
-
-    err = __tail(list, &x);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int dbll_search(const struct dbll_node *list, dbll_compar_t compar,
-                const struct dbll_node *kx, struct dbll_node **_x)
+int dbll_node_search(const struct dbll_node *pool, dbll_compar_t compar,
+                     const struct dbll_node *kx, struct dbll_node **_x)
 {
     struct dbll_node *x;
 
-    for (x = list->next; x != list; x = x->next) {
+    for (x = pool->next; x != pool; x = x->next) {
         if (compar(kx, x) == 0) {
             if (_x != NULL)
                 *_x = x;
@@ -176,30 +24,211 @@ int dbll_search(const struct dbll_node *list, dbll_compar_t compar,
     return -ENOENT;
 }
 
-void dbll_forward(struct dbll_node *list, dbll_do_each_t do_each, void *priv)
+void dbll_node_forward(struct dbll_node *pool, dbll_do_each_t do_each,
+                       void *priv)
 {
     struct dbll_node *x;
 
-    for (x = list->next; x != list; x = x->next)
+    for (x = pool->next; x != pool; x = x->next)
         do_each(x, priv);
 }
 
-void dbll_backward(struct dbll_node *list, dbll_do_each_t do_each, void *priv)
+void dbll_node_backward(struct dbll_node *pool, dbll_do_each_t do_each,
+                        void *priv)
 {
     struct dbll_node *x;
 
-    for (x = list->prev; x != list; x = x->prev)
+    for (x = pool->prev; x != pool; x = x->prev)
         do_each(x, priv);
 }
 
-void dbll_reverse(struct dbll_node *list)
+void dbll_node_reverse(struct dbll_node *pool)
 {
-    struct dbll_node *x = list->next;
+    struct dbll_node *x = pool->next;
 
     do {
-        struct dbll_node *y = x->next;
+        struct dbll_node *z = x->next;
 
-        dbll_unlink(y);
-        dbll_link_next(list, y);
-    } while (x->next != list);
+        dbll_node_unlink(z);
+        dbll_node_link_next(pool, z);
+    } while (x->next != pool);
+}
+
+static int __head(const struct dbll *list, struct dbll_node **_x)
+{
+    if (dbll_is_empty(list))
+        return -ENODATA;
+
+    *_x = list->pool.next;
+
+    return 0;
+}
+
+static int __tail(const struct dbll *list, struct dbll_node **_x)
+{
+    if (dbll_is_empty(list))
+        return -ENODATA;
+
+    *_x = list->pool.prev;
+
+    return 0;
+}
+
+static int __get(const struct dbll *list, ssize_t n, struct dbll_node **_x,
+                 struct dbll_node **_p)
+{
+    const struct dbll_node *pool = &list->pool;
+    struct dbll_node *x = pool->next;
+    struct dbll_node *p = (struct dbll_node *)pool;
+
+    if (dbll_is_empty(list))
+        return -ENODATA;
+
+    while (n-- > 0 && x != pool) {
+        p = x;
+        x = x->next;
+    }
+
+    if (x == pool)
+        return -ERANGE;
+
+    *_x = x;
+    *_p = p;
+
+    return 0;
+}
+
+int dbll_insert(struct dbll *list, size_t n, struct dbll_node *x)
+{
+    struct dbll_node *y, *p;
+    int err;
+
+    err = __get(list, n, &y, &p);
+    if (err != 0)
+        return err;
+
+    dbll_node_link_next(p, x);
+    list->count++;
+
+    return 0;
+}
+
+int dbll_remvoe(struct dbll *list, size_t n, struct dbll_node **_x)
+{
+    struct dbll_node *x, *p;
+    int err;
+
+    err = __get(list, n, &x, &p);
+    if (err != 0)
+        return err;
+
+    dbll_node_unlink(x);
+    list->count--;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int dbll_remove_head(struct dbll *list, struct dbll_node **_x)
+{
+    struct dbll_node *x;
+    int err;
+
+    err = __head(list, &x);
+    if (err != 0)
+        return err;
+
+    dbll_node_unlink(x);
+    list->count--;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int dbll_remove_tail(struct dbll *list, struct dbll_node **_x)
+{
+    struct dbll_node *x;
+    int err;
+
+    err = __tail(list, &x);
+    if (err != 0)
+        return err;
+
+    dbll_node_unlink(x);
+    list->count--;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int dbll_get(const struct dbll *list, size_t n, struct dbll_node **_x)
+{
+    struct dbll_node *x, *p;
+    int err;
+
+    err = __get(list, n, &x, &p);
+    if (err != 0)
+        return err;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int dbll_get_head(const struct dbll *list, struct dbll_node **_x)
+{
+    struct dbll_node *x;
+    int err;
+
+    err = __head(list, &x);
+    if (err != 0)
+        return err;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int dbll_get_tail(const struct dbll *list, struct dbll_node **_x)
+{
+    struct dbll_node *x;
+    int err;
+
+    err = __tail(list, &x);
+    if (err != 0)
+        return err;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int dbll_search(const struct dbll *list, dbll_compar_t compar,
+                const struct dbll_node *kx, struct dbll_node **_x)
+{
+    return dbll_node_search(&list->pool, compar, kx, _x);
+}
+
+void dbll_forward(struct dbll *list, dbll_do_each_t do_each, void *priv)
+{
+    dbll_node_forward(&list->pool, do_each, priv);
+}
+
+void dbll_backward(struct dbll *list, dbll_do_each_t do_each, void *priv)
+{
+    dbll_node_backward(&list->pool, do_each, priv);
+}
+
+void dbll_reverse(struct dbll *list)
+{
+    dbll_node_reverse(&list->pool);
 }
