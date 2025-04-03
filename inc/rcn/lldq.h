e738563b3078a736f5c6ffc70a3ddd47024d1c38
YOUNGJIN JOO <neoelec@gmail.com>
2025-04-03 22:47:21 +0900

    lldq: updated, according to the changes of dbll
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- inc/rcn/lldq.h
+++ inc/rcn/lldq.h
@@ -4,56 +4,122 @@
  */
 
 /* Linked List Deque */
-#ifndef __LLDQ_H__
-#define __LLDQ_H__
+#ifndef __DEQUE_H__
+#define __DEQUE_H__
 
 #include "dbll.h"
 
-struct lldq {
-    size_t count;
-    struct dbll_node *head;
-};
-
-static inline void lldq_initialize(struct lldq *deque)
+static inline void lldq_initialize(struct dbll *deque)
 {
-    deque->count = 0;
-    deque->head = NULL;
+    dbll_initialize(deque);
 }
 
-static inline bool lldq_is_empty(const struct lldq *deque)
+static inline size_t lldq_count(const struct dbll *deque)
 {
-    return deque->head == NULL;
+    return dbll_count(deque);
 }
-
-static inline size_t lldq_count(const struct lldq *deque)
+static inline bool lldq_is_empty(const struct dbll *deque)
 {
-    return deque->count;
+    return dbll_is_empty(deque);
 }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-extern size_t lldq_count(const struct lldq *deque);
-extern void lldq_insert(struct lldq *deque, ssize_t n, struct dbll_node *x);
-extern void lldq_push_front(struct lldq *deque, struct dbll_node *x);
-extern void lldq_push_rear(struct lldq *deque, struct dbll_node *x);
-extern int lldq_remove(struct lldq *deque, ssize_t n, struct dbll_node **_x);
-extern int lldq_pop_front(struct lldq *deque, struct dbll_node **_x);
-extern int lldq_pop_rear(struct lldq *deque, struct dbll_node **_x);
-extern int lldq_get(const struct lldq *deque, ssize_t n, struct dbll_node **_x);
-extern int lldq_front(const struct lldq *deque, struct dbll_node **_x);
-extern int lldq_rear(const struct lldq *deque, struct dbll_node **_x);
-extern int lldq_search(const struct lldq *deque, dbll_compar_t compar,
-                       const struct dbll_node *kx, struct dbll_node **_x);
-extern void lldq_forward(struct lldq *deque, dbll_do_each_t do_each,
-                         void *priv);
-extern void lldq_backward(struct lldq *deque, dbll_do_each_t do_each,
-                          void *priv);
-extern void lldq_reverse(struct lldq *deque);
-extern void lldq_rotate(struct lldq *deque, ssize_t n);
+static inline int lldq_insert(struct dbll *deque, ssize_t n,
+                              struct dbll_node *x)
+{
+    ssize_t count = deque->count;
 
-#ifdef __cplusplus
+    if (n >= 0)
+        return dbll_insert(deque, n % count, x);
+    else
+        return lldq_insert(deque, count + n % count, x);
 }
-#endif
 
-#endif /* __LLDQ_H__ */
+static inline void lldq_push_front(struct dbll *deque, struct dbll_node *x)
+{
+    dbll_insert_head(deque, x);
+}
+
+static inline void lldq_push_rear(struct dbll *deque, struct dbll_node *x)
+{
+    dbll_insert_tail(deque, x);
+}
+
+static inline int lldq_remove(struct dbll *deque, ssize_t n,
+                              struct dbll_node **_x)
+{
+    ssize_t count = deque->count;
+
+    if (n >= 0)
+        return dbll_remvoe(deque, n % count, _x);
+    else
+        return lldq_remove(deque, count + n % count, _x);
+}
+
+static inline int lldq_pop_front(struct dbll *deque, struct dbll_node **_x)
+{
+    return dbll_remove_head(deque, _x);
+}
+
+static inline int lldq_pop_rear(struct dbll *deque, struct dbll_node **_x)
+{
+    return dbll_remove_tail(deque, _x);
+}
+
+static inline int lldq_get(const struct dbll *deque, ssize_t n,
+                           struct dbll_node **_x)
+{
+    ssize_t count = deque->count;
+
+    if (n >= 0)
+        return dbll_get(deque, n % count, _x);
+    else
+        return lldq_get(deque, count + n % count, _x);
+}
+
+static inline int lldq_front(const struct dbll *deque, struct dbll_node **_x)
+{
+    return dbll_get_head(deque, _x);
+}
+
+static inline int lldq_rear(const struct dbll *deque, struct dbll_node **_x)
+{
+    return dbll_get_tail(deque, _x);
+}
+
+static inline int lldq_search(const struct dbll *deque, dbll_compar_t compar,
+                              const struct dbll_node *kx, struct dbll_node **_x)
+{
+    return dbll_search(deque, compar, kx, _x);
+}
+
+static inline void lldq_forward(struct dbll *deque, dbll_do_each_t do_each,
+                                void *priv)
+{
+    dbll_forward(deque, do_each, priv);
+}
+
+static inline void lldq_backward(struct dbll *deque, dbll_do_each_t do_each,
+                                 void *priv)
+{
+    dbll_backward(deque, do_each, priv);
+}
+
+static inline void lldq_reverse(struct dbll *deque)
+{
+    dbll_reverse(deque);
+}
+
+static inline void lldq_rotate(struct dbll *deque, ssize_t n)
+{
+    ssize_t count = deque->count;
+    struct dbll_node *y;
+
+    if (count == 0)
+        return;
+
+    lldq_get(deque, n, &y);
+    dbll_node_unlink(&deque->pool);
+    dbll_node_link_prev(y, &deque->pool);
+}
+
+#endif /* __DEQUE_H__ */
