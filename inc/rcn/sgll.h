f6fd3637d5aa525f1c9a025bc2c91fe3d68c1b24
YOUNGJIN JOO <neoelec@gmail.com>
2025-04-06 12:54:39 +0900

    sgll: introduce sgll container
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- inc/rcn/sgll.h
+++ inc/rcn/sgll.h
@@ -18,49 +18,84 @@ struct sgll_node {
     struct sgll_node *next;
 };
 
-static inline void sgll_initialize(struct sgll_node *list)
+static inline void sgll_node_initialize(struct sgll_node *list)
 {
     list->next = NULL;
 }
 
-static inline bool sgll_is_empty(const struct sgll_node *list)
+static inline bool sgll_node_is_orphaned(const struct sgll_node *list)
 {
     return list->next == NULL;
 }
 
-static inline void sgll_link_next(struct sgll_node *p, struct sgll_node *x)
+static inline void sgll_node_link_next(struct sgll_node *p, struct sgll_node *x)
 {
     x->next = p->next;
     p->next = x;
 }
 
-static inline void sgll_insert_head(struct sgll_node *list, struct sgll_node *x)
+static inline void sgll_node_unlink_next(struct sgll_node *p)
 {
-    sgll_link_next(list, x);
+    struct sgll_node *x = p->next;
+
+    if (x != NULL) {
+        p->next = x->next;
+        x->next = NULL;
+    }
+}
+
+struct sgll {
+    size_t count;
+    struct sgll_node head;
+};
+
+static inline void sgll_initialize(struct sgll *list)
+{
+    sgll_node_initialize(&list->head);
+    list->count = 0;
+}
+
+static inline bool sgll_is_empty(const struct sgll *list)
+{
+    return list->count == 0;
+}
+
+static inline size_t sgll_count(const struct sgll *list)
+{
+    return list->count;
+}
+
+static inline void sgll_insert_head(struct sgll *list, struct sgll_node *x)
+{
+    sgll_node_link_next(&list->head, x);
+    list->count++;
 }
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern size_t sgll_count(const struct sgll_node *list);
-extern int sgll_unlink_next(struct sgll_node *pos, struct sgll_node **_x);
-extern int sgll_unlink(struct sgll_node *list, struct sgll_node *z);
-extern int sgll_insert(struct sgll_node *list, size_t n, struct sgll_node *x);
-extern void sgll_insert_tail(struct sgll_node *list, struct sgll_node *x);
-extern int sgll_remvoe(struct sgll_node *list, size_t n, struct sgll_node **_x);
-extern int sgll_remove_head(struct sgll_node *list, struct sgll_node **_x);
-extern int sgll_remove_tail(struct sgll_node *list, struct sgll_node **_x);
-extern int sgll_get(const struct sgll_node *list, size_t n,
-                    struct sgll_node **_x);
-extern int sgll_get_tail(const struct sgll_node *list, struct sgll_node **_x);
-extern int sgll_get_head(const struct sgll_node *list, struct sgll_node **_x);
-extern int sgll_search(const struct sgll_node *list, sgll_compar_t compar,
+extern int sgll_node_search(const struct sgll_node *head, sgll_compar_t compar,
+                            const struct sgll_node *kx, struct sgll_node **_x,
+                            struct sgll_node **_p);
+extern void sgll_node_forward(struct sgll_node *head, sgll_do_each_t do_each,
+                              void *priv);
+extern void sgll_node_reverse(struct sgll_node *head);
+
+extern size_t sgll_count_debug(const struct sgll *list);
+extern int sgll_insert(struct sgll *list, size_t n, struct sgll_node *x);
+extern int sgll_insert_tail(struct sgll *list, struct sgll_node *x);
+extern int sgll_remvoe(struct sgll *list, size_t n, struct sgll_node **_x);
+extern int sgll_remove_head(struct sgll *list, struct sgll_node **_x);
+extern int sgll_remove_tail(struct sgll *list, struct sgll_node **_x);
+extern int sgll_get(const struct sgll *list, size_t n, struct sgll_node **_x);
+extern int sgll_get_head(const struct sgll *list, struct sgll_node **_x);
+extern int sgll_get_tail(const struct sgll *list, struct sgll_node **_x);
+extern int sgll_search(const struct sgll *list, sgll_compar_t compar,
                        const struct sgll_node *kx, struct sgll_node **_x,
                        struct sgll_node **_p);
-extern void sgll_forward(struct sgll_node *list, sgll_do_each_t do_each,
-                         void *priv);
-extern void sgll_reverse(struct sgll_node *list);
+extern void sgll_forward(struct sgll *list, sgll_do_each_t do_each, void *priv);
+extern void sgll_reverse(struct sgll *list);
 
 #ifdef __cplusplus
 }
--- src/sgll.c
+++ src/sgll.c
@@ -7,225 +7,19 @@
 
 #include <rcn/sgll.h>
 
-static int __head(const struct sgll_node *list, struct sgll_node **_x)
-{
-    *_x = list->next;
-
-    return sgll_is_empty(list) ? -ENODATA : 0;
-}
-
-static int __tail(const struct sgll_node *list, struct sgll_node **_x,
-                  struct sgll_node **_p)
-{
-    struct sgll_node *x = list->next;
-    struct sgll_node *p = (struct sgll_node *)list;
-
-    if (x == NULL) {
-        *_x = p;
-        *_p = p;
-        return -ERANGE;
-    }
-
-    while (x->next != NULL) {
-        p = x;
-        x = x->next;
-    }
-
-    *_x = x;
-    *_p = p;
-
-    return 0;
-}
-
-static void __unlink_next(struct sgll_node *p)
-{
-    struct sgll_node *x = p->next;
-
-    p->next = x->next;
-    x->next = NULL;
-}
-
-static int __get(const struct sgll_node *list, ssize_t n, struct sgll_node **_x,
-                 struct sgll_node **_p)
-{
-    const struct sgll_node *x = list->next;
-    const struct sgll_node *p = list;
-
-    for (; n > 0 && x != NULL; --n) {
-        p = x;
-        x = x->next;
-    }
-
-    if (x == NULL)
-        return -ERANGE;
-
-    *_x = (struct sgll_node *)x;
-    *_p = (struct sgll_node *)p;
-
-    return 0;
-}
-
-size_t sgll_count(const struct sgll_node *list)
+int sgll_node_search(const struct sgll_node *head, sgll_compar_t compar,
+                     const struct sgll_node *kx, struct sgll_node **_x,
+                     struct sgll_node **_p)
 {
     const struct sgll_node *x;
-    size_t count = 0;
-
-    for (x = list->next; x != NULL; x = x->next)
-        count++;
-
-    return count;
-}
-
-int sgll_unlink_next(struct sgll_node *p, struct sgll_node **_x)
-{
-    if (p->next == NULL)
-        return -ERANGE;
-
-    if (_x != NULL)
-        *_x = p->next;
-
-    __unlink_next(p);
-
-    return 0;
-}
-
-int sgll_unlink(struct sgll_node *list, struct sgll_node *z)
-{
-    struct sgll_node *p = list;
-
-    while (p->next != z && p->next != NULL)
-        p = p->next;
-
-    if (p->next != z)
-        return -ERANGE;
-
-    __unlink_next(p);
-
-    return 0;
-}
-
-int sgll_insert(struct sgll_node *list, size_t n, struct sgll_node *x)
-{
-    struct sgll_node *y, *p;
-    int err;
-
-    err = __get(list, n, &y, &p);
-    if (err != 0)
-        return -ERANGE;
-
-    sgll_link_next(p, x);
-
-    return 0;
-}
-
-void sgll_insert_tail(struct sgll_node *list, struct sgll_node *x)
-{
-    struct sgll_node *y, *p;
-
-    __tail(list, &y, &p);
-    sgll_link_next(y, x);
-}
-
-int sgll_remvoe(struct sgll_node *list, size_t n, struct sgll_node **_x)
-{
-    struct sgll_node *x, *p;
-    int err;
-
-    err = __get(list, n, &x, &p);
-    if (err != 0)
-        return -ERANGE;
-
-    __unlink_next(p);
-
-    if (_x == NULL)
-        *_x = x;
-
-    return 0;
-}
-
-int sgll_remove_head(struct sgll_node *list, struct sgll_node **_x)
-{
-    struct sgll_node *x;
-    int err;
-
-    err = __head(list, &x);
-
-    list->next = x != NULL ? x->next : NULL;
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int sgll_remove_tail(struct sgll_node *list, struct sgll_node **_x)
-{
-    struct sgll_node *x, *p;
-    int err;
-
-    err = __tail(list, &x, &p);
-
-    if (p != NULL)
-        p->next = NULL;
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int sgll_get(const struct sgll_node *list, size_t n, struct sgll_node **_x)
-{
-    struct sgll_node *x, *p;
-    int err;
-
-    err = __get(list, n, &x, &p);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int sgll_get_head(const struct sgll_node *list, struct sgll_node **_x)
-{
-    struct sgll_node *x;
-    int err;
-
-    err = __head(list, &x);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int sgll_get_tail(const struct sgll_node *list, struct sgll_node **_x)
-{
-    struct sgll_node *x, *p;
-    int err;
-
-    err = __tail(list, &x, &p);
-
-    if (_x != NULL)
-        *_x = x;
-
-    return err;
-}
-
-int sgll_search(const struct sgll_node *list, sgll_compar_t compar,
-                const struct sgll_node *kx, struct sgll_node **_x,
-                struct sgll_node **_p)
-{
-    const struct sgll_node *x;
-    const struct sgll_node *p = list;
+    const struct sgll_node *p = head;
     int ret;
 
-    if (sgll_is_empty(list)) {
-        x = list;
+    if (sgll_node_is_orphaned(head)) {
+        x = head;
         ret = -ENOENT;
     } else {
-        x = list->next;
+        x = head->next;
         while (x != NULL && compar(kx, x) != 0) {
             p = x;
             x = x->next;
@@ -243,25 +37,222 @@ int sgll_search(const struct sgll_node *list, sgll_compar_t compar,
     return ret;
 }
 
-void sgll_forward(struct sgll_node *list, sgll_do_each_t do_each, void *priv)
+void sgll_node_forward(struct sgll_node *head, sgll_do_each_t do_each,
+                       void *priv)
 {
     struct sgll_node *x;
 
-    for (x = list->next; x != NULL; x = x->next)
+    for (x = head->next; x != NULL; x = x->next)
         do_each(x, priv);
 }
 
-void sgll_reverse(struct sgll_node *list)
+void sgll_node_reverse(struct sgll_node *head)
 {
-    struct sgll_node *x = list->next;
+    struct sgll_node *x = head->next;
 
-    list->next = NULL;
+    head->next = NULL;
 
     while (x != NULL) {
         struct sgll_node *y = x->next;
 
-        x->next = list->next;
-        list->next = x;
+        x->next = head->next;
+        head->next = x;
         x = y;
     }
+}
+
+static int __tail(const struct sgll *list, struct sgll_node **_x,
+                  struct sgll_node **_p)
+{
+    const struct sgll_node *head = &list->head;
+    struct sgll_node *x = head->next;
+    struct sgll_node *p = (struct sgll_node *)head;
+
+    if (x == NULL)
+        x = p;
+
+    while (x->next != NULL) {
+        p = x;
+        x = x->next;
+    }
+
+    *_x = x;
+    *_p = p;
+
+    return 0;
+}
+
+static int __get(const struct sgll *list, ssize_t n, struct sgll_node **_x,
+                 struct sgll_node **_p)
+{
+    const struct sgll_node *head = &list->head;
+    const struct sgll_node *x = head->next;
+    const struct sgll_node *p = head;
+
+    for (; n > 0 && x != NULL; --n) {
+        p = x;
+        x = x->next;
+    }
+
+    if (x == NULL)
+        return -ERANGE;
+
+    *_x = (struct sgll_node *)x;
+    *_p = (struct sgll_node *)p;
+
+    return 0;
+}
+
+size_t sgll_count_debug(const struct sgll *list)
+{
+    const struct sgll_node *head = &list->head;
+    const struct sgll_node *x;
+    size_t count = 0;
+
+    for (x = head->next; x != NULL; x = x->next)
+        count++;
+
+    return count;
+}
+
+int sgll_insert(struct sgll *list, size_t n, struct sgll_node *x)
+{
+    struct sgll_node *y, *p;
+    int err;
+
+    err = __get(list, n, &y, &p);
+    if (err != 0)
+        return err;
+
+    sgll_node_link_next(p, x);
+    list->count++;
+
+    return 0;
+}
+
+int sgll_insert_tail(struct sgll *list, struct sgll_node *y)
+{
+    struct sgll_node *x, *p;
+    int err;
+
+    err = __tail(list, &x, &p);
+    if (err != 0)
+        return err;
+
+    sgll_node_link_next(x, y);
+    list->count++;
+
+    return 0;
+}
+
+int sgll_remvoe(struct sgll *list, size_t n, struct sgll_node **_x)
+{
+    struct sgll_node *x, *p;
+    int err;
+
+    err = __get(list, n, &x, &p);
+    if (err != 0)
+        return err;
+
+    sgll_node_unlink_next(p);
+    list->count--;
+
+    if (_x == NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int sgll_remove_head(struct sgll *list, struct sgll_node **_x)
+{
+    struct sgll_node *head = &list->head;
+
+    if (sgll_is_empty(list))
+        return -ENODATA;
+
+    if (_x != NULL)
+        *_x = head->next;
+
+    sgll_node_unlink_next(head);
+    list->count--;
+
+    return 0;
+}
+
+int sgll_remove_tail(struct sgll *list, struct sgll_node **_x)
+{
+    struct sgll_node *x, *p;
+    int err;
+
+    err = __tail(list, &x, &p);
+    if (err != 0)
+        return err;
+
+    sgll_node_unlink_next(p);
+    list->count--;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int sgll_get(const struct sgll *list, size_t n, struct sgll_node **_x)
+{
+    struct sgll_node *x, *p;
+    int err;
+
+    err = __get(list, n, &x, &p);
+    if (err != 0)
+        return err;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int sgll_get_head(const struct sgll *list, struct sgll_node **_x)
+{
+    const struct sgll_node *head = &list->head;
+
+    if (sgll_is_empty(list))
+        return -ENODATA;
+
+    if (_x != NULL)
+        *_x = head->next;
+
+    return 0;
+}
+
+int sgll_get_tail(const struct sgll *list, struct sgll_node **_x)
+{
+    struct sgll_node *x, *p;
+    int err;
+
+    err = __tail(list, &x, &p);
+    if (err != 0)
+        return err;
+
+    if (_x != NULL)
+        *_x = x;
+
+    return 0;
+}
+
+int sgll_search(const struct sgll *list, sgll_compar_t compar,
+                const struct sgll_node *kx, struct sgll_node **_x,
+                struct sgll_node **_p)
+{
+    return sgll_node_search(&list->head, compar, kx, _x, _p);
+}
+
+void sgll_forward(struct sgll *list, sgll_do_each_t do_each, void *priv)
+{
+    return sgll_node_forward(&list->head, do_each, priv);
+}
+
+void sgll_reverse(struct sgll *list)
+{
+    sgll_node_reverse(&list->head);
 }
