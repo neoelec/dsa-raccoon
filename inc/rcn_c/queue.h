c7ef4015620670a497f590d31b0f8ae4c2727b18
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:50:50 +0000

    rcn_c/queue: Queue (Array)
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/queue.h
@@ -0,0 +1,118 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Queue (Array) */
+#ifndef __RCN_C_QUEUE_H__
+#define __RCN_C_QUEUE_H__
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+struct queue {
+    size_t nr_entries_;
+    void **entry_;
+    size_t front_;
+    size_t back_;
+    size_t size_;
+};
+
+static inline void queue_clear(struct queue *self)
+{
+    self->front_ = self->nr_entries_ - 1;
+    self->back_ = 0;
+    self->size_ = 0;
+}
+
+static inline void queue_init(struct queue *self, size_t nr_entries,
+                              void **entry)
+{
+    self->nr_entries_ = nr_entries;
+    self->entry_ = entry;
+    queue_clear(self);
+}
+
+static inline size_t queue_size(const struct queue *self)
+{
+    return self->size_;
+}
+
+static inline size_t queue_max_size(const struct queue *self)
+{
+    return self->nr_entries_;
+}
+
+static inline bool queue_empty(const struct queue *self)
+{
+    return queue_size(self) == 0;
+}
+
+static inline bool queue_full(const struct queue *self)
+{
+    return queue_size(self) == self->nr_entries_;
+}
+
+static inline void *queue_front(const struct queue *self)
+{
+    return queue_empty(self) ? NULL : self->entry_[self->front_];
+}
+
+static inline void *queue_back(const struct queue *self)
+{
+    return queue_empty(self) ? NULL : self->entry_[self->back_];
+}
+
+static inline void queue_push(struct queue *self, void *e)
+{
+    if (queue_full(self)) {
+        return;
+    }
+    self->back_ = self->back_ == 0 ? self->nr_entries_ - 1 : self->back_ - 1;
+    self->entry_[self->back_] = e;
+    self->size_++;
+}
+
+static inline void *queue_pop(struct queue *self)
+{
+    if (queue_empty(self)) {
+        return NULL;
+    }
+    void *e = self->entry_[self->front_];
+    self->front_ = self->front_ == 0 ? self->nr_entries_ - 1 : self->front_ - 1;
+    self->size_--;
+    return e;
+}
+
+static inline void queue_swap(struct queue *self, struct queue *other)
+{
+    struct queue tmp;
+    memcpy(&tmp, self, sizeof(tmp));
+    memcpy(self, other, sizeof(tmp));
+    memcpy(other, &tmp, sizeof(tmp));
+}
+
+static inline int queue_alloc_entry(struct queue *self)
+{
+    self->entry_ = (void **)calloc(self->nr_entries_, sizeof(*self->entry_));
+    return self->entry_ == NULL ? -ENOMEM : 0;
+}
+
+static inline void queue_free_entry(struct queue *self)
+{
+    free(self->entry_);
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_QUEUE_H__ */
---
+++ test/c_queue_00/main.cpp
@@ -0,0 +1,125 @@
+#include "gtest/gtest.h"
+#include "rcn_c/queue.h"
+
+class QueueTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        queue_ = new rcn_c::queue;
+        queue_init(queue_, nr_entries_, nullptr);
+        queue_alloc_entry(queue_);
+    }
+
+    void TearDown() override
+    {
+        queue_free_entry(queue_);
+        delete queue_;
+    }
+
+    static const size_t nr_entries_ = 5;
+    rcn_c::queue *queue_;
+};
+
+TEST_F(QueueTest, Init)
+{
+    ASSERT_EQ(queue_size(queue_), 0);
+    static const size_t nr_entries = nr_entries_;
+    ASSERT_EQ(queue_max_size(queue_), nr_entries);
+    ASSERT_TRUE(queue_empty(queue_));
+    ASSERT_FALSE(queue_full(queue_));
+}
+
+TEST_F(QueueTest, Empty)
+{
+    ASSERT_TRUE(queue_empty(queue_));
+    queue_push(queue_, (void *)1);
+    ASSERT_FALSE(queue_empty(queue_));
+}
+
+TEST_F(QueueTest, Full)
+{
+    int v[nr_entries_];
+    for (size_t i = 0; i < nr_entries_; ++i) {
+        v[i] = (int)(i + 1);
+        queue_push(queue_, &v[i]);
+    }
+    ASSERT_TRUE(queue_full(queue_));
+    queue_pop(queue_);
+    ASSERT_FALSE(queue_full(queue_));
+}
+
+TEST_F(QueueTest, PushPop)
+{
+    int a = 1, b = 2, c = 3;
+    queue_push(queue_, &a);
+    queue_push(queue_, &b);
+    queue_push(queue_, &c);
+
+    ASSERT_EQ(queue_size(queue_), 3);
+    static const size_t nr_entries = nr_entries_;
+    ASSERT_EQ(queue_max_size(queue_), nr_entries);
+
+    ASSERT_EQ(queue_front(queue_), &a);
+    ASSERT_EQ(queue_back(queue_), &c);
+
+    ASSERT_EQ(queue_pop(queue_), &a);
+    ASSERT_EQ(queue_pop(queue_), &b);
+    ASSERT_EQ(queue_pop(queue_), &c);
+    ASSERT_TRUE(queue_empty(queue_));
+}
+
+TEST_F(QueueTest, Clear)
+{
+    int v[nr_entries_];
+    for (size_t i = 0; i < nr_entries_; ++i) {
+        v[i] = (int)(i + 1);
+        queue_push(queue_, &v[i]);
+    }
+    queue_clear(queue_);
+    ASSERT_TRUE(queue_empty(queue_));
+}
+
+TEST_F(QueueTest, FrontBack)
+{
+    int a = 1, b = 2;
+    queue_push(queue_, &a);
+    queue_push(queue_, &b);
+
+    ASSERT_EQ(queue_front(queue_), &a);
+    ASSERT_EQ(queue_back(queue_), &b);
+
+    queue_pop(queue_);
+    ASSERT_EQ(queue_front(queue_), &b);
+    ASSERT_EQ(queue_back(queue_), &b);
+}
+
+TEST_F(QueueTest, Swap)
+{
+    rcn_c::queue other_queue;
+    queue_init(&other_queue, nr_entries_, nullptr);
+    queue_alloc_entry(&other_queue);
+
+    int a = 1, b = 2;
+    queue_push(queue_, &a);
+    queue_push(queue_, &b);
+
+    int c = 3, d = 4;
+    queue_push(&other_queue, &c);
+    queue_push(&other_queue, &d);
+
+    queue_swap(queue_, &other_queue);
+
+    ASSERT_EQ(queue_front(queue_), &c);
+    ASSERT_EQ(queue_back(queue_), &d);
+
+    ASSERT_EQ(queue_front(&other_queue), &a);
+    ASSERT_EQ(queue_back(&other_queue), &b);
+
+    queue_free_entry(&other_queue);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
