a0713e6ef3a709818af3448de3670f750b1cb02b
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:58:31 +0000

    rcn_cpp/babylonian: The Babylonian Method
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_cpp/babylonian_sqrt.h
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0+
+//
+// Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+//
+
+// The Babylonian Method
+#ifndef __RCN_CPP_BABYLONIAN_SQRT_H__
+#define __RCN_CPP_BABYLONIAN_SQRT_H__
+
+namespace rcn_cpp
+{
+
+template <typename T>
+static T babylonian_sqrt(T y, T tolerance)
+{
+    T x = y, x_next;
+    if (y <= 0) {
+        return 0;
+    }
+    do {
+        x_next = (x + y / x) / 2;
+        if ((x_next > x ? x_next - x : x - x_next) <= tolerance) {
+            break;
+        }
+        x = x_next;
+    } while (true);
+    return x_next;
+}
+
+}
+
+#endif // __RCN_CPP_BABYLONIAN_SQRT_H__
