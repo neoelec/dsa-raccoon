67bb40b31cf3215af4a3d7c3fa94c47aaf1e459b
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:54:32 +0000

    rcn_c/eratosthenes: Sieve of Eratosthenes
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/eratosthenes.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2024-2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Sieve of Eratosthenes */
+#ifndef __RCN_C_ERATOSTHENES_H__
+#define __RCN_C_ERATOSTHENES_H__
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+static inline bool *eratosthenes(unsigned long long n)
+{
+    bool *sieve = (bool *)calloc(n + 1, sizeof(*sieve));
+    for (unsigned long long j = 0; j <= n; ++j) {
+        sieve[j] = true;
+    }
+    sieve[0] = sieve[1] = false;
+    for (unsigned long long i = 2; i * i <= n; ++i) {
+        if (!sieve[i]) {
+            continue;
+        }
+        for (unsigned long long j = i + i; j <= n; j += i) {
+            sieve[j] = false;
+        }
+    }
+    return sieve;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RCN_C_ERATOSTHENES_H__ */
---
+++ test/c_eratosthenes_00/main.cpp
@@ -0,0 +1,61 @@
+#include <cstring>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "rcn_c/eratosthenes.h"
+
+TEST(eratosthenesTest, BasicTest)
+{
+    bool expected[] = { false, false, true,  true,  false, true,  false, true,
+                        false, false, false, true,  false, true,  false, false,
+                        false, true,  false, true,  false, false, false, true,
+                        false, false, false, false, false, true };
+    bool *sieve = rcn_c::eratosthenes(29);
+    EXPECT_TRUE(0 == std::memcmp(sieve, expected, sizeof(expected)));
+    free(sieve);
+}
+
+TEST(eratosthenesTest, EmptyTest)
+{
+    bool expected[] = { false, false };
+    bool *sieve = rcn_c::eratosthenes(1);
+    EXPECT_TRUE(0 == std::memcmp(sieve, expected, sizeof(expected)));
+    free(sieve);
+}
+
+TEST(eratosthenesTest, LargerNumberTest)
+{
+    bool *sieve = rcn_c::eratosthenes(100);
+    ASSERT_EQ(sieve[2], true);
+    ASSERT_EQ(sieve[3], true);
+    ASSERT_EQ(sieve[5], true);
+    ASSERT_EQ(sieve[7], true);
+    ASSERT_EQ(sieve[97], true);
+    ASSERT_EQ(sieve[4], false);
+    ASSERT_EQ(sieve[6], false);
+    ASSERT_EQ(sieve[10], false);
+    ASSERT_EQ(sieve[100], false);
+    free(sieve);
+}
+
+TEST(eratosthenesTest, CheckPrimesUpto100)
+{
+    std::vector<int> expected_primes = { 2,  3,  5,  7,  11, 13, 17, 19, 23,
+                                         29, 31, 37, 41, 43, 47, 53, 59, 61,
+                                         67, 71, 73, 79, 83, 89, 97 };
+    bool *sieve = rcn_c::eratosthenes(100);
+    std::vector<int> actual_primes;
+    for (size_t i = 2; i <= 100; ++i) {
+        if (sieve[i]) {
+            actual_primes.push_back(i);
+        }
+    }
+    ASSERT_EQ(expected_primes, actual_primes);
+    free(sieve);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
