251ba73278ee5d670bf992b9207c535b5c71fbb1
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:52:11 +0000

    rcn_c/slhash: Hash Table - Separate Chaining (Singly Linked List)
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/slhash.h
@@ -0,0 +1,145 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Hash Table - Separate Chaining (Singly Linked List) */
+#ifndef __RCN_C_SLHASH_H__
+#define __RCN_C_SLHASH_H__
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "slist.h"
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+struct slhash {
+    size_t sz_bucket_;
+    size_t (*key_hash_)(const void *ke);
+    int (*compar_)(const void *ke, const void *in_table);
+    struct slist *bucket_;
+};
+
+static inline size_t __slhash_index(const struct slhash *table, const void *ke)
+{
+    return table->key_hash_(ke) % table->sz_bucket_;
+}
+
+static inline void slhash_clear(struct slhash *self)
+{
+    if (self->bucket_ == NULL) {
+        return;
+    }
+    for (size_t i = 0; i < self->sz_bucket_; ++i) {
+        slist_clear(&self->bucket_[i]);
+    }
+}
+
+static inline void slhash_init(
+    struct slhash *self, size_t sz_bucket, size_t (*key_hash)(const void *ke),
+    int (*compar)(const void *ke, const void *in_table), struct slist *bucket)
+{
+    self->sz_bucket_ = sz_bucket;
+    self->key_hash_ = key_hash;
+    self->compar_ = compar;
+    self->bucket_ = bucket;
+    slhash_clear(self);
+}
+
+static inline size_t slhash_buckets(const struct slhash *self)
+{
+    return self->sz_bucket_;
+}
+
+static inline struct slnode *slhash_begin(const struct slhash *self, size_t n)
+{
+    return slist_begin(&self->bucket_[n]);
+}
+
+static inline const struct slnode *slhash_end(const struct slhash *self,
+                                              size_t n)
+{
+    return slist_end(&self->bucket_[n]);
+}
+
+static inline bool slhash_empty(const struct slhash *self)
+{
+    for (size_t i = 0; i < slhash_buckets(self); ++i) {
+        if (!slist_empty(&self->bucket_[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static inline size_t slhash_size(const struct slhash *self)
+{
+    size_t count = 0;
+    for (size_t i = 0; i < slhash_buckets(self); ++i) {
+        count += slist_size(&self->bucket_[i]);
+    }
+    return count;
+}
+
+static inline void slhash_insert(struct slhash *self, struct slnode *x, void *e)
+{
+    size_t n = __slhash_index(self, e);
+    slist_push_front(&self->bucket_[n], x, e);
+}
+
+static inline struct slnode *slhash_find(const struct slhash *self,
+                                         const void *ke)
+{
+    struct slnode *x;
+    size_t n = __slhash_index(self, ke);
+    for (x = slhash_begin(self, n); x != slhash_end(self, n); x = x->next_) {
+        if (self->compar_(ke, x->entry_) == 0) {
+            return x;
+        }
+    }
+    return NULL;
+}
+
+static inline void *slhash_at(const struct slhash *self, const void *ke)
+{
+    struct slnode *x = slhash_find(self, ke);
+    return x == NULL ? NULL : x->entry_;
+}
+
+static inline size_t slhash_count(const struct slhash *self, const void *ke)
+{
+    size_t n = __slhash_index(self, ke);
+    return slist_size(&self->bucket_[n]);
+}
+
+static inline void slhash_swap(struct slhash *self, struct slhash *other)
+{
+    struct slhash tmp;
+    memcpy(&tmp, self, sizeof(tmp));
+    memcpy(self, other, sizeof(tmp));
+    memcpy(other, &tmp, sizeof(tmp));
+}
+
+static inline int slhash_alloc_bucket(struct slhash *self)
+{
+    self->bucket_ =
+        (struct slist *)calloc(self->sz_bucket_, sizeof(*self->bucket_));
+    slhash_clear(self);
+    return self->bucket_ == NULL ? -ENOMEM : 0;
+}
+
+static inline void slhash_free_bucket(struct slhash *self)
+{
+    free(self->bucket_);
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_SLHASH_H__ */
---
+++ test/c_slhash_00/main.cpp
@@ -0,0 +1,195 @@
+#include "gtest/gtest.h"
+#include "rcn_c/slhash.h"
+
+struct TestData {
+    TestData(int value)
+        : value_(value)
+    {
+    }
+    rcn_c::slnode node_;
+    int value_;
+};
+
+class SLHashTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        table_ = new rcn_c::slhash;
+        slhash_init(table_, sz_bucket_, _KeyHash, _ValueCompare, nullptr);
+        slhash_alloc_bucket(table_);
+    }
+
+    void TearDown() override
+    {
+        slhash_free_bucket(table_);
+        delete table_;
+    }
+
+    static size_t _KeyHash(const void *_ke)
+    {
+        auto *ke = (const TestData *)_ke;
+        return ke->value_;
+    }
+
+    static int _ValueCompare(const void *_ke, const void *_in_table)
+    {
+        auto ke = (const TestData *)_ke;
+        auto *in_table = (const TestData *)_in_table;
+        if (ke->value_ < in_table->value_)
+            return -1;
+        if (ke->value_ > in_table->value_)
+            return 1;
+        return 0;
+    }
+
+    const size_t sz_bucket_ = 53;
+    rcn_c::slhash *table_;
+};
+
+TEST_F(SLHashTest, Init)
+{
+    const size_t sz_bucket = sz_bucket_;
+    ASSERT_EQ(slhash_buckets(table_), sz_bucket);
+    ASSERT_EQ(slhash_size(table_), 0);
+}
+
+TEST_F(SLHashTest, Find)
+{
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    slhash_insert(table_, &data1->node_, data1);
+    slhash_insert(table_, &data2->node_, data2);
+
+    auto key = new TestData(10);
+    ASSERT_EQ(slhash_find(table_, key), &data1->node_);
+    key->value_ = 20;
+    ASSERT_EQ(slhash_find(table_, key), &data2->node_);
+
+    delete data1;
+    delete data2;
+}
+
+TEST_F(SLHashTest, InsertAndAt)
+{
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    slhash_insert(table_, &data1->node_, data1);
+    slhash_insert(table_, &data2->node_, data2);
+
+    auto key = new TestData(10);
+    ASSERT_EQ(slhash_at(table_, key), data1);
+    key->value_ = 20;
+    ASSERT_EQ(slhash_at(table_, key), data2);
+
+    delete key;
+    delete data1;
+    delete data2;
+}
+
+TEST_F(SLHashTest, EmptyAndSize)
+{
+    ASSERT_TRUE(slhash_empty(table_));
+    ASSERT_EQ(slhash_size(table_), 0);
+
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    slhash_insert(table_, &data1->node_, data1);
+    slhash_insert(table_, &data2->node_, data2);
+
+    ASSERT_FALSE(slhash_empty(table_));
+    ASSERT_EQ(slhash_size(table_), 2);
+
+    delete data1;
+    delete data2;
+}
+
+TEST_F(SLHashTest, Clear)
+{
+    ASSERT_TRUE(slhash_empty(table_));
+    ASSERT_EQ(slhash_size(table_), 0);
+
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    slhash_insert(table_, &data1->node_, data1);
+    slhash_insert(table_, &data2->node_, data2);
+
+    slhash_clear(table_);
+
+    ASSERT_TRUE(slhash_empty(table_));
+    ASSERT_EQ(slhash_size(table_), 0);
+
+    delete data1;
+    delete data2;
+}
+
+TEST_F(SLHashTest, Count)
+{
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    slhash_insert(table_, &data1->node_, data1);
+    slhash_insert(table_, &data2->node_, data2);
+
+    auto key = new TestData(10);
+    ASSERT_EQ(slhash_count(table_, key), 1);
+    key->value_ = 20;
+    ASSERT_EQ(slhash_count(table_, key), 1);
+}
+
+TEST_F(SLHashTest, Swap)
+{
+    rcn_c::slhash other_table_;
+
+    slhash_init(&other_table_, 23, _KeyHash, _ValueCompare, nullptr);
+    slhash_alloc_bucket(&other_table_);
+
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    slhash_insert(table_, &data1->node_, data1);
+    slhash_insert(&other_table_, &data2->node_, data2);
+
+    ASSERT_EQ(slhash_size(table_), 1);
+    ASSERT_EQ(slhash_size(&other_table_), 1);
+
+    slhash_swap(table_, &other_table_);
+
+    ASSERT_EQ(slhash_size(table_), 1);
+    ASSERT_EQ(slhash_size(&other_table_), 1);
+    auto key = new TestData(20);
+    ASSERT_EQ(slhash_at(table_, key), data2);
+    key->value_ = 10;
+    ASSERT_EQ(slhash_at(&other_table_, key), data1);
+
+    delete data1;
+    delete data2;
+    slhash_alloc_bucket(&other_table_);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
