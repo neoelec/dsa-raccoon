ce1443abfd2ea28c9a34cdd43307e70f59568965
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:52:44 +0000

    rcn/dlhash: Hash Table - Separate Chaining (Doubly Linked List)
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/dlhash.h
@@ -0,0 +1,160 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Hash Table - Separate Chaining (Doubly Linked List) */
+#ifndef __RCN_C_DLHASH_H__
+#define __RCN_C_DLHASH_H__
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "dlist.h"
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+struct dlhash {
+    size_t sz_bucket_;
+    size_t (*key_hash_)(const void *ke);
+    int (*compar_)(const void *ke, const void *in_table);
+    struct dlist *bucket_;
+};
+
+static inline size_t __dlhash_index(const struct dlhash *self, const void *ke)
+{
+    return self->key_hash_(ke) % self->sz_bucket_;
+}
+
+static inline void dlhash_clear(struct dlhash *self)
+{
+    if (self->bucket_ == NULL) {
+        return;
+    }
+    for (size_t i = 0; i < self->sz_bucket_; ++i) {
+        dlist_clear(&self->bucket_[i]);
+    }
+}
+
+static inline void dlhash_init(
+    struct dlhash *self, size_t sz_bucket, size_t (*key_hash)(const void *ke),
+    int (*compar)(const void *ke, const void *in_table), struct dlist *bucket)
+{
+    self->sz_bucket_ = sz_bucket;
+    self->key_hash_ = key_hash;
+    self->compar_ = compar;
+    self->bucket_ = bucket;
+    dlhash_clear(self);
+}
+
+static inline size_t dlhash_buckets(const struct dlhash *self)
+{
+    return self->sz_bucket_;
+}
+
+static inline struct dlnode *dlhash_begin(const struct dlhash *self, size_t n)
+{
+    return dlist_begin(&self->bucket_[n]);
+}
+
+static inline const struct dlnode *dlhash_end(const struct dlhash *self,
+                                              size_t n)
+{
+    return dlist_end(&self->bucket_[n]);
+}
+
+static inline bool dlhash_empty(const struct dlhash *self)
+{
+    for (size_t i = 0; i < dlhash_buckets(self); ++i) {
+        if (!dlist_empty(&self->bucket_[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static inline size_t dlhash_size(const struct dlhash *self)
+{
+    size_t count = 0;
+    for (size_t i = 0; i < dlhash_buckets(self); ++i) {
+        count += dlist_size(&self->bucket_[i]);
+    }
+    return count;
+}
+
+static inline void dlhash_insert(struct dlhash *self, struct dlnode *x, void *e)
+{
+    size_t n = __dlhash_index(self, e);
+    dlist_push_front(&self->bucket_[n], x, e);
+}
+
+static inline void *dlhash_erase(struct dlhash *self, struct dlnode *x)
+{
+    return dlist_erase(x);
+}
+
+static inline struct dlnode *dlhash_find(const struct dlhash *self,
+                                         const void *ke)
+{
+    struct dlnode *x;
+    size_t n = __dlhash_index(self, ke);
+    for (x = dlhash_begin(self, n); x != dlhash_end(self, n); x = x->next_) {
+        if (self->compar_(ke, x->entry_) == 0) {
+            return x;
+        }
+    }
+    return NULL;
+}
+
+static inline void *dlhash_at(const struct dlhash *self, const void *ke)
+{
+    struct dlnode *x = dlhash_find(self, ke);
+    return x == NULL ? NULL : x->entry_;
+}
+
+static inline struct dlnode *dlhash_remove(struct dlhash *self, const void *ke)
+{
+    struct dlnode *x = dlhash_find(self, ke);
+    if (x == NULL) {
+        return NULL;
+    }
+    dlhash_erase(self, x);
+    return x;
+}
+
+static inline size_t dlhash_count(const struct dlhash *self, const void *ke)
+{
+    size_t n = __dlhash_index(self, ke);
+    return dlist_size(&self->bucket_[n]);
+}
+
+static inline void dlhash_swap(struct dlhash *self, struct dlhash *other)
+{
+    struct dlhash tmp;
+    memcpy(&tmp, self, sizeof(tmp));
+    memcpy(self, other, sizeof(tmp));
+    memcpy(other, &tmp, sizeof(tmp));
+}
+
+static inline int dlhash_alloc_bucket(struct dlhash *self)
+{
+    self->bucket_ =
+        (struct dlist *)calloc(self->sz_bucket_, sizeof(*self->bucket_));
+    dlhash_clear(self);
+    return self->bucket_ == NULL ? -ENOMEM : 0;
+}
+
+static inline void dlhash_free_bucket(struct dlhash *self)
+{
+    free(self->bucket_);
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_DLHASH_H__ */
---
+++ test/c_dlhash_00/main.cpp
@@ -0,0 +1,224 @@
+#include "gtest/gtest.h"
+#include "rcn_c/dlhash.h"
+
+struct TestData {
+    TestData(int value)
+        : value_(value)
+    {
+    }
+    rcn_c::dlnode node_;
+    int value_;
+};
+
+class DLHashTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        table_ = new rcn_c::dlhash;
+        dlhash_init(table_, sz_bucket_, _KeyHash, _ValueCompare, nullptr);
+        dlhash_alloc_bucket(table_);
+    }
+
+    void TearDown() override
+    {
+        for (size_t i = 0; i < dlhash_buckets(table_); i++) {
+            while (dlhash_begin(table_, i) != dlhash_end(table_, i)) {
+                rcn_c::dlnode *x = dlhash_begin(table_, i);
+                dlhash_erase(table_, x);
+                delete (TestData *)x;
+            }
+        }
+
+        dlhash_free_bucket(table_);
+        delete table_;
+    }
+
+    static size_t _KeyHash(const void *_ke)
+    {
+        auto *ke = (const TestData *)_ke;
+        return ke->value_;
+    }
+
+    static int _ValueCompare(const void *_ke, const void *_in_table)
+    {
+        auto ke = (const TestData *)_ke;
+        auto *in_table = (const TestData *)_in_table;
+        if (ke->value_ < in_table->value_)
+            return -1;
+        if (ke->value_ > in_table->value_)
+            return 1;
+        return 0;
+    }
+
+    const size_t sz_bucket_ = 53;
+    rcn_c::dlhash *table_;
+};
+
+TEST_F(DLHashTest, Init)
+{
+    const size_t sz_bucket = sz_bucket_;
+    ASSERT_EQ(dlhash_buckets(table_), sz_bucket);
+    ASSERT_EQ(dlhash_size(table_), 0);
+}
+
+TEST_F(DLHashTest, Find)
+{
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    dlhash_insert(table_, &data1->node_, data1);
+    dlhash_insert(table_, &data2->node_, data2);
+
+    auto key = new TestData(10);
+    ASSERT_EQ(dlhash_find(table_, key), &data1->node_);
+    key->value_ = 20;
+    ASSERT_EQ(dlhash_find(table_, key), &data2->node_);
+}
+
+TEST_F(DLHashTest, InsertAndAt)
+{
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    dlhash_insert(table_, &data1->node_, data1);
+    dlhash_insert(table_, &data2->node_, data2);
+
+    auto key = new TestData(10);
+    ASSERT_EQ(dlhash_at(table_, key), data1);
+    key->value_ = 20;
+    ASSERT_EQ(dlhash_at(table_, key), data2);
+
+    delete key;
+}
+
+TEST_F(DLHashTest, Erase)
+{
+    auto *data = new TestData(10);
+
+    ASSERT_NE(data, nullptr);
+
+    dlhash_insert(table_, &data->node_, data);
+    auto key = new TestData(10);
+    ASSERT_NE(dlhash_at(table_, key), nullptr);
+
+    dlhash_erase(table_, &data->node_);
+    ASSERT_EQ(dlhash_at(table_, key), nullptr);
+
+    delete key;
+}
+
+TEST_F(DLHashTest, Remove)
+{
+    auto *data = new TestData(10);
+
+    ASSERT_NE(data, nullptr);
+
+    dlhash_insert(table_, &data->node_, data);
+    auto key = new TestData(10);
+    ASSERT_NE(dlhash_at(table_, key), nullptr);
+
+    dlhash_remove(table_, key);
+    ASSERT_EQ(dlhash_at(table_, key), nullptr);
+    delete key;
+}
+
+TEST_F(DLHashTest, EmptyAndSize)
+{
+    ASSERT_TRUE(dlhash_empty(table_));
+    ASSERT_EQ(dlhash_size(table_), 0);
+
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    dlhash_insert(table_, &data1->node_, data1);
+    dlhash_insert(table_, &data2->node_, data2);
+
+    ASSERT_FALSE(dlhash_empty(table_));
+    ASSERT_EQ(dlhash_size(table_), 2);
+}
+
+TEST_F(DLHashTest, Clear)
+{
+    ASSERT_TRUE(dlhash_empty(table_));
+    ASSERT_EQ(dlhash_size(table_), 0);
+
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    dlhash_insert(table_, &data1->node_, data1);
+    dlhash_insert(table_, &data2->node_, data2);
+
+    dlhash_clear(table_);
+
+    ASSERT_TRUE(dlhash_empty(table_));
+    ASSERT_EQ(dlhash_size(table_), 0);
+
+    delete data1;
+    delete data2;
+}
+
+TEST_F(DLHashTest, Count)
+{
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    dlhash_insert(table_, &data1->node_, data1);
+    dlhash_insert(table_, &data2->node_, data2);
+
+    auto key = new TestData(10);
+    ASSERT_EQ(dlhash_count(table_, key), 1);
+    key->value_ = 20;
+    ASSERT_EQ(dlhash_count(table_, key), 1);
+}
+
+TEST_F(DLHashTest, Swap)
+{
+    rcn_c::dlhash other_table_;
+    dlhash_init(&other_table_, 23, _KeyHash, _ValueCompare, nullptr);
+    dlhash_alloc_bucket(&other_table_);
+
+    auto *data1 = new TestData(10);
+    auto *data2 = new TestData(20);
+
+    ASSERT_NE(data1, nullptr);
+    ASSERT_NE(data2, nullptr);
+
+    dlhash_insert(table_, &data1->node_, data1);
+    dlhash_insert(&other_table_, &data2->node_, data2);
+
+    ASSERT_EQ(dlhash_size(table_), 1);
+    ASSERT_EQ(dlhash_size(&other_table_), 1);
+
+    dlhash_swap(table_, &other_table_);
+
+    ASSERT_EQ(dlhash_size(table_), 1);
+    ASSERT_EQ(dlhash_size(&other_table_), 1);
+    auto key = new TestData(20);
+    ASSERT_EQ(dlhash_at(table_, key), data2);
+    key->value_ = 10;
+    ASSERT_EQ(dlhash_at(&other_table_, key), data1);
+
+    delete data1;
+    dlhash_alloc_bucket(&other_table_);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
