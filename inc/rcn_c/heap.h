6c013a57385149e1543351ec6831729f10771b79
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:51:25 +0000

    rcn_c/heap: Heap (Array)
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/heap.h
@@ -0,0 +1,158 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Heap (Array) */
+#ifndef __RCN_C_HEAP_H__
+#define __RCN_C_HEAP_H__
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+struct heap {
+    size_t nr_entries_;
+    int (*compar_)(const void *ke, const void *in_heap);
+    void **entry_;
+    size_t size_;
+};
+
+static inline size_t __heap_parent(size_t pos)
+{
+    return (pos - 1) / 2;
+}
+
+static inline size_t __heap_left(size_t pos)
+{
+    return (pos * 2) + 1;
+}
+
+static inline int __heap_compar(const struct heap *self, size_t ke_i,
+                                size_t in_heap_i)
+{
+    return self->compar_(self->entry_[ke_i], self->entry_[in_heap_i]);
+}
+
+static inline void __heap_swap(struct heap *self, size_t ke_i, size_t in_heap_i)
+{
+    void *tmp = self->entry_[ke_i];
+    self->entry_[ke_i] = self->entry_[in_heap_i];
+    self->entry_[in_heap_i] = tmp;
+}
+
+static inline void heap_clear(struct heap *self)
+{
+    self->size_ = 0;
+}
+
+static inline void heap_init(struct heap *self, size_t nr_entries,
+                             int (*compar)(const void *ke, const void *in_heap),
+                             void **entry)
+{
+    self->nr_entries_ = nr_entries;
+    self->compar_ = compar;
+    self->entry_ = entry;
+    heap_clear(self);
+}
+
+static inline size_t heap_size(const struct heap *self)
+{
+    return self->size_;
+}
+
+static inline size_t heap_max_size(const struct heap *self)
+{
+    return self->nr_entries_;
+}
+
+static inline bool heap_empty(const struct heap *self)
+{
+    return heap_size(self) == 0;
+}
+
+static inline bool heap_full(const struct heap *self)
+{
+    return heap_size(self) == self->nr_entries_;
+}
+
+static inline void *heap_top(const struct heap *self)
+{
+    return heap_empty(self) ? NULL : self->entry_[0];
+}
+
+static inline void heap_push(struct heap *self, void *e)
+{
+    if (heap_full(self)) {
+        return;
+    }
+    ssize_t pos, parent;
+    pos = self->size_++;
+    self->entry_[pos] = e;
+    parent = __heap_parent(pos);
+    while (pos > 0 && __heap_compar(self, pos, parent) < 0) {
+        __heap_swap(self, pos, parent);
+        pos = parent;
+        parent = __heap_parent(pos);
+    }
+}
+
+static inline void *heap_pop(struct heap *self)
+{
+    if (heap_empty(self)) {
+        return NULL;
+    }
+    size_t pos, child, left, right;
+    void *e = self->entry_[0];
+    self->entry_[0] = self->entry_[--self->size_];
+    pos = 0;
+    left = __heap_left(pos);
+    while (left < self->size_) {
+        right = left + 1;
+        if (right == self->size_) {
+            child = left;
+        } else {
+            child = __heap_compar(self, left, right) < 0 ? left : right;
+        }
+        if (__heap_compar(self, pos, child) < 0) {
+            break;
+        }
+        __heap_swap(self, pos, child);
+        pos = child;
+        left = __heap_left(pos);
+    }
+    return e;
+}
+
+static inline void heap_swap(struct heap *self, struct heap *other)
+{
+    struct heap tmp;
+    memcpy(&tmp, self, sizeof(tmp));
+    memcpy(self, other, sizeof(tmp));
+    memcpy(other, &tmp, sizeof(tmp));
+}
+
+static inline int heap_alloc_entry(struct heap *self)
+{
+    self->entry_ = (void **)calloc(self->nr_entries_, sizeof(*self->entry_));
+    return self->entry_ == NULL ? -ENOMEM : 0;
+}
+
+static inline void heap_free_entry(struct heap *self)
+{
+    free(self->entry_);
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_HEAP_H__ */
---
+++ test/c_heap_00/main.cpp
@@ -0,0 +1,130 @@
+#include "gtest/gtest.h"
+#include "rcn_c/heap.h"
+
+class HeapTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        heap_ = new rcn_c::heap;
+        heap_init(heap_, nr_entries_, _ValueCompare, nullptr);
+        heap_alloc_entry(heap_);
+    }
+
+    void TearDown() override
+    {
+        heap_free_entry(heap_);
+        delete heap_;
+    }
+
+    static int _ValueCompare(const void *_ke, const void *_in_heap)
+    {
+        int ke = *(int *)_ke;
+        int in_heap = *(int *)_in_heap;
+        if (ke < in_heap)
+            return -1;
+        if (ke > in_heap)
+            return 1;
+        return 0;
+    }
+
+    static const size_t nr_entries_ = 5;
+    rcn_c::heap *heap_;
+};
+
+TEST_F(HeapTest, Init)
+{
+    ASSERT_EQ(heap_size(heap_), 0);
+    static const size_t nr_entries = nr_entries_;
+    ASSERT_EQ(heap_max_size(heap_), nr_entries);
+    ASSERT_TRUE(heap_empty(heap_));
+    ASSERT_FALSE(heap_full(heap_));
+}
+
+TEST_F(HeapTest, Empty)
+{
+    ASSERT_TRUE(heap_empty(heap_));
+    heap_push(heap_, (void *)1);
+    ASSERT_FALSE(heap_empty(heap_));
+}
+
+TEST_F(HeapTest, Full)
+{
+    int v[nr_entries_];
+    for (size_t i = 0; i < nr_entries_; ++i) {
+        v[i] = (int)(i + 1);
+        heap_push(heap_, &v[i]);
+    }
+    ASSERT_TRUE(heap_full(heap_));
+    heap_pop(heap_);
+    ASSERT_FALSE(heap_full(heap_));
+}
+
+TEST_F(HeapTest, PushPop)
+{
+    int a = 1, b = 2, c = 3;
+    heap_push(heap_, &a);
+    heap_push(heap_, &b);
+    heap_push(heap_, &c);
+
+    ASSERT_EQ(heap_size(heap_), 3);
+    static const size_t nr_entries = nr_entries_;
+    ASSERT_EQ(heap_max_size(heap_), nr_entries);
+
+    ASSERT_EQ(heap_top(heap_), &a);
+
+    ASSERT_EQ(heap_pop(heap_), &a);
+    ASSERT_EQ(heap_pop(heap_), &b);
+    ASSERT_EQ(heap_pop(heap_), &c);
+    ASSERT_TRUE(heap_empty(heap_));
+}
+
+TEST_F(HeapTest, Clear)
+{
+    int v[nr_entries_];
+    for (size_t i = 0; i < nr_entries_; ++i) {
+        v[i] = (int)(i + 1);
+        heap_push(heap_, &v[i]);
+    }
+    heap_clear(heap_);
+    ASSERT_TRUE(heap_empty(heap_));
+}
+
+TEST_F(HeapTest, FrontBack)
+{
+    int a = 1, b = 2;
+    heap_push(heap_, &a);
+    heap_push(heap_, &b);
+
+    ASSERT_EQ(heap_top(heap_), &a);
+
+    heap_pop(heap_);
+    ASSERT_EQ(heap_top(heap_), &b);
+}
+
+TEST_F(HeapTest, Swap)
+{
+    rcn_c::heap other_heap;
+    heap_init(&other_heap, nr_entries_, _ValueCompare, nullptr);
+    heap_alloc_entry(&other_heap);
+
+    int a = 1, b = 2;
+    heap_push(heap_, &a);
+    heap_push(heap_, &b);
+
+    int c = 3, d = 4;
+    heap_push(&other_heap, &c);
+    heap_push(&other_heap, &d);
+
+    heap_swap(heap_, &other_heap);
+
+    ASSERT_EQ(heap_top(heap_), &c);
+    ASSERT_EQ(heap_top(&other_heap), &a);
+
+    heap_free_entry(&other_heap);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
