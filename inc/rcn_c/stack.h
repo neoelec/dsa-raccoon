9c4b1f8121a21a44921c033cff8feb38683451ed
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:50:23 +0000

    rcn_c/stack: Stack (Array)
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/stack.h
@@ -0,0 +1,107 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Stack (Array) */
+#ifndef __RCN_C_STACK_H__
+#define __RCN_C_STACK_H__
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+struct stack {
+    size_t nr_entries_;
+    void **entry_;
+    size_t top_;
+};
+
+static inline void stack_clear(struct stack *self)
+{
+    self->top_ = -1;
+}
+
+static inline void stack_init(struct stack *self, size_t nr_entries,
+                              void **entry)
+{
+    self->nr_entries_ = nr_entries;
+    self->entry_ = entry;
+    stack_clear(self);
+}
+
+static inline size_t stack_size(const struct stack *self)
+{
+    return self->top_ + 1;
+}
+
+static inline size_t stack_max_size(const struct stack *self)
+{
+    return self->nr_entries_;
+}
+
+static inline bool stack_empty(const struct stack *self)
+{
+    return stack_size(self) == 0;
+}
+
+static inline bool stack_full(const struct stack *self)
+{
+    return stack_size(self) == self->nr_entries_;
+}
+
+static inline void *stack_top(const struct stack *self)
+{
+    if (stack_empty(self)) {
+        return NULL;
+    }
+    return self->entry_[self->top_];
+}
+
+static inline void stack_push(struct stack *self, void *e)
+{
+    if (stack_size(self) == self->nr_entries_) {
+        return;
+    }
+    self->entry_[++self->top_] = e;
+}
+
+static inline void *stack_pop(struct stack *self)
+{
+    if (stack_empty(self)) {
+        return NULL;
+    }
+    return self->entry_[self->top_--];
+}
+
+static inline void stack_swap(struct stack *self, struct stack *other)
+{
+    struct stack tmp;
+    memcpy(&tmp, self, sizeof(tmp));
+    memcpy(self, other, sizeof(tmp));
+    memcpy(other, &tmp, sizeof(tmp));
+}
+
+static inline int stack_alloc_entry(struct stack *self)
+{
+    self->entry_ = (void **)calloc(self->nr_entries_, sizeof(*self->entry_));
+    return self->entry_ == NULL ? -ENOMEM : 0;
+}
+
+static inline void stack_free_entry(struct stack *self)
+{
+    free(self->entry_);
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_STACK_H__ */
---
+++ test/c_stack_00/main.cpp
@@ -0,0 +1,108 @@
+#include "gtest/gtest.h"
+#include "rcn_c/stack.h"
+
+class StackTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        stack_ = new rcn_c::stack;
+        stack_init(stack_, nr_entries_, nullptr);
+        stack_alloc_entry(stack_);
+    }
+
+    void TearDown() override
+    {
+        stack_free_entry(stack_);
+        delete stack_;
+    }
+
+    static const size_t nr_entries_ = 5;
+    rcn_c::stack *stack_;
+};
+
+TEST_F(StackTest, StackInitClear)
+{
+    ASSERT_EQ(stack_size(stack_), 0);
+    static const size_t nr_entries = nr_entries_;
+    ASSERT_EQ(stack_max_size(stack_), nr_entries);
+    ASSERT_TRUE(stack_empty(stack_));
+    ASSERT_FALSE(stack_full(stack_));
+}
+
+TEST_F(StackTest, StackPushPop)
+{
+    int a = 1, b = 2, c = 3;
+
+    stack_push(stack_, &a);
+    ASSERT_EQ(stack_size(stack_), 1);
+    ASSERT_FALSE(stack_empty(stack_));
+    ASSERT_EQ(stack_top(stack_), &a);
+
+    stack_push(stack_, &b);
+    ASSERT_EQ(stack_size(stack_), 2);
+    ASSERT_EQ(stack_top(stack_), &b);
+
+    stack_push(stack_, &c);
+    ASSERT_EQ(stack_size(stack_), 3);
+    ASSERT_EQ(stack_top(stack_), &c);
+
+    ASSERT_EQ(stack_pop(stack_), &c);
+    ASSERT_EQ(stack_size(stack_), 2);
+    ASSERT_EQ(stack_top(stack_), &b);
+
+    ASSERT_EQ(stack_pop(stack_), &b);
+    ASSERT_EQ(stack_size(stack_), 1);
+    ASSERT_EQ(stack_top(stack_), &a);
+
+    ASSERT_EQ(stack_pop(stack_), &a);
+    ASSERT_EQ(stack_size(stack_), 0);
+    ASSERT_TRUE(stack_empty(stack_));
+    ASSERT_EQ(stack_top(stack_), nullptr);
+}
+
+TEST_F(StackTest, StackOverflow)
+{
+    int v[nr_entries_];
+    for (size_t i = 0; i < nr_entries_; ++i) {
+        v[i] = (int)(i + 1);
+        stack_push(stack_, &v[i]);
+    }
+    static const size_t nr_entries = nr_entries_;
+    ASSERT_EQ(stack_size(stack_), nr_entries);
+    int a = 1;
+    stack_push(stack_, &a);
+    ASSERT_EQ(stack_size(stack_), nr_entries);
+}
+
+TEST_F(StackTest, StackUnderflow)
+{
+    ASSERT_TRUE(stack_empty(stack_));
+    ASSERT_EQ(stack_pop(stack_), nullptr);
+}
+
+TEST_F(StackTest, StackSwap)
+{
+    rcn_c::stack other_stack;
+    stack_init(&other_stack, nr_entries_, nullptr);
+    stack_alloc_entry(&other_stack);
+
+    int a = 1, b = 2;
+    stack_push(stack_, &a);
+    stack_push(&other_stack, &b);
+
+    stack_swap(stack_, &other_stack);
+
+    ASSERT_EQ(stack_size(stack_), 1);
+    ASSERT_EQ(stack_top(stack_), &b);
+
+    ASSERT_EQ(stack_size(&other_stack), 1);
+    ASSERT_EQ(stack_top(&other_stack), &a);
+
+    stack_free_entry(&other_stack);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
