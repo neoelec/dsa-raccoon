8c76e530bad8897d2c0320fd06b29ad9257e9008
YOUNGJIN JOO <neoelec@gmail.com>
2025-05-04 12:51:08 +0000

    rch_c:deque: Deque (Array)
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/deque.h
@@ -0,0 +1,142 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Deque (Array) */
+#ifndef __RCN_C_DEQUE_H__
+#define __RCN_C_DEQUE_H__
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+struct deque {
+    size_t nr_entries_;
+    void **entry_;
+    size_t front_;
+    size_t back_;
+    size_t size_;
+};
+
+static inline void deque_clear(struct deque *self)
+{
+    self->front_ = self->nr_entries_ - 1;
+    self->back_ = 0;
+    self->size_ = 0;
+}
+
+static inline void deque_init(struct deque *self, size_t nr_entries,
+                              void **entry)
+{
+    self->nr_entries_ = nr_entries;
+    self->entry_ = entry;
+    deque_clear(self);
+}
+
+static inline size_t deque_size(const struct deque *self)
+{
+    return self->size_;
+}
+
+static inline size_t deque_max_size(const struct deque *self)
+{
+    return self->nr_entries_;
+}
+
+static inline bool deque_empty(const struct deque *self)
+{
+    return deque_size(self) == 0;
+}
+
+static inline bool deque_full(const struct deque *self)
+{
+    return deque_size(self) == self->nr_entries_;
+}
+
+static inline void *deque_front(const struct deque *self)
+{
+    return deque_empty(self) ? NULL : self->entry_[self->front_];
+}
+
+static inline void *deque_back(const struct deque *self)
+{
+    return deque_empty(self) ? NULL : self->entry_[self->back_];
+}
+
+static inline void deque_push_front(struct deque *self, void *e)
+{
+    if (deque_full(self)) {
+        return;
+    }
+    self->front_++;
+    if (self->front_ == self->nr_entries_) {
+        self->front_ = 0;
+    }
+    self->entry_[self->front_] = e;
+    self->size_++;
+}
+
+static inline void deque_push_back(struct deque *self, void *e)
+{
+    if (deque_full(self)) {
+        return;
+    }
+    self->back_ = self->back_ == 0 ? self->nr_entries_ - 1 : self->back_ - 1;
+    self->entry_[self->back_] = e;
+    self->size_++;
+}
+
+static inline void *deque_pop_front(struct deque *self)
+{
+    if (deque_empty(self)) {
+        return NULL;
+    }
+    void *e = self->entry_[self->front_];
+    self->front_ = self->front_ == 0 ? self->nr_entries_ - 1 : self->front_ - 1;
+    self->size_--;
+    return e;
+}
+
+static inline void *deque_pop_back(struct deque *self)
+{
+    if (deque_empty(self)) {
+        return NULL;
+    }
+    void *e = self->entry_[self->back_++];
+    self->back_ = self->back_ == self->nr_entries_ ? 0 : self->back_;
+    self->size_--;
+    return e;
+}
+
+static inline void deque_swap(struct deque *self, struct deque *other)
+{
+    struct deque tmp;
+    memcpy(&tmp, self, sizeof(tmp));
+    memcpy(self, other, sizeof(tmp));
+    memcpy(other, &tmp, sizeof(tmp));
+}
+
+static inline int deque_alloc_entry(struct deque *self)
+{
+    self->entry_ = (void **)calloc(self->nr_entries_, sizeof(*self->entry_));
+    return self->entry_ == NULL ? -ENOMEM : 0;
+}
+
+static inline void deque_free_entry(struct deque *self)
+{
+    free(self->entry_);
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_DEQUE_H__ */
---
+++ test/c_deque_00/main.cpp
@@ -0,0 +1,155 @@
+#include "gtest/gtest.h"
+#include "rcn_c/deque.h"
+
+class DequeTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        deque_ = new rcn_c::deque;
+        deque_init(deque_, nr_entries_, nullptr);
+        deque_alloc_entry(deque_);
+    }
+
+    void TearDown() override
+    {
+        deque_free_entry(deque_);
+        delete deque_;
+    }
+
+    const size_t nr_entries_ = 5;
+    rcn_c::deque *deque_;
+};
+
+TEST_F(DequeTest, Init)
+{
+    ASSERT_EQ(deque_size(deque_), 0);
+    static const size_t nr_entries = nr_entries_;
+    ASSERT_EQ(deque_max_size(deque_), nr_entries);
+    ASSERT_TRUE(deque_empty(deque_));
+    ASSERT_FALSE(deque_full(deque_));
+}
+
+TEST_F(DequeTest, PushFront)
+{
+    int a = 1, b = 2, c = 3;
+    deque_push_front(deque_, &a);
+    ASSERT_EQ(deque_size(deque_), 1);
+    ASSERT_EQ(deque_front(deque_), &a);
+
+    deque_push_front(deque_, &b);
+    ASSERT_EQ(deque_size(deque_), 2);
+    ASSERT_EQ(deque_front(deque_), &b);
+
+    deque_push_front(deque_, &c);
+    ASSERT_EQ(deque_size(deque_), 3);
+    ASSERT_EQ(deque_front(deque_), &c);
+}
+
+TEST_F(DequeTest, PushBack)
+{
+    int a = 1, b = 2, c = 3;
+    deque_push_back(deque_, &a);
+    ASSERT_EQ(deque_size(deque_), 1);
+    ASSERT_EQ(deque_back(deque_), &a);
+
+    deque_push_back(deque_, &b);
+    ASSERT_EQ(deque_size(deque_), 2);
+    ASSERT_EQ(deque_back(deque_), &b);
+
+    deque_push_back(deque_, &c);
+    ASSERT_EQ(deque_size(deque_), 3);
+    ASSERT_EQ(deque_back(deque_), &c);
+}
+
+TEST_F(DequeTest, PopFront)
+{
+    int a = 1, b = 2, c = 3;
+    deque_push_front(deque_, &a);
+    deque_push_front(deque_, &b);
+    deque_push_front(deque_, &c);
+
+    ASSERT_EQ(deque_pop_front(deque_), &c);
+    ASSERT_EQ(deque_size(deque_), 2);
+    ASSERT_EQ(deque_pop_front(deque_), &b);
+    ASSERT_EQ(deque_size(deque_), 1);
+    ASSERT_EQ(deque_pop_front(deque_), &a);
+    ASSERT_EQ(deque_size(deque_), 0);
+    ASSERT_TRUE(deque_empty(deque_));
+}
+
+TEST_F(DequeTest, PopBack)
+{
+    int a = 1, b = 2, c = 3;
+    deque_push_back(deque_, &a);
+    deque_push_back(deque_, &b);
+    deque_push_back(deque_, &c);
+
+    ASSERT_EQ(deque_pop_back(deque_), &c);
+    ASSERT_EQ(deque_size(deque_), 2);
+    ASSERT_EQ(deque_pop_back(deque_), &b);
+    ASSERT_EQ(deque_size(deque_), 1);
+    ASSERT_EQ(deque_pop_back(deque_), &a);
+    ASSERT_EQ(deque_size(deque_), 0);
+    ASSERT_TRUE(deque_empty(deque_));
+}
+
+TEST_F(DequeTest, FrontAndBack)
+{
+    int a = 1, b = 2, c = 3;
+    deque_push_front(deque_, &a);
+    ASSERT_EQ(deque_front(deque_), &a);
+    ASSERT_EQ(deque_back(deque_), &a);
+
+    deque_push_back(deque_, &b);
+    ASSERT_EQ(deque_front(deque_), &a);
+    ASSERT_EQ(deque_back(deque_), &b);
+
+    deque_push_front(deque_, &c);
+    ASSERT_EQ(deque_front(deque_), &c);
+    ASSERT_EQ(deque_back(deque_), &b);
+}
+
+TEST_F(DequeTest, FullAndClear)
+{
+    int a = 1, b = 2, c = 3, d = 4, e = 5;
+    deque_push_front(deque_, &a);
+    deque_push_front(deque_, &b);
+    deque_push_front(deque_, &c);
+    deque_push_front(deque_, &d);
+    deque_push_front(deque_, &e);
+
+    ASSERT_TRUE(deque_full(deque_));
+
+    deque_clear(deque_);
+    ASSERT_TRUE(deque_empty(deque_));
+}
+
+TEST_F(DequeTest, Swap)
+{
+    rcn_c::deque other_deque;
+    deque_init(&other_deque, nr_entries_, nullptr);
+    deque_alloc_entry(&other_deque);
+
+    int a = 1, b = 2;
+    deque_push_front(deque_, &a);
+    deque_push_back(&other_deque, &b);
+
+    deque_swap(deque_, &other_deque);
+
+    ASSERT_EQ(deque_front(deque_), &b);
+    ASSERT_EQ(deque_back(deque_), &b);
+    ASSERT_EQ(deque_size(deque_), 1);
+
+    ASSERT_EQ(deque_front(&other_deque), &a);
+    ASSERT_EQ(deque_back(&other_deque), &a);
+    ASSERT_EQ(deque_size(&other_deque), 1);
+    ASSERT_EQ(deque_back(&other_deque), &a);
+
+    deque_free_entry(&other_deque);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
