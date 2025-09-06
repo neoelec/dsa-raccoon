f0918a4009765b15a3d2da820284b82c4494ee3d
YOUNGJIN JOO <neoelec@gmail.com>
2025-09-06 20:20:31 +0900

    CONTAINER_OF macro
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- inc/rcn_c/common.h
+++ inc/rcn_c/common.h
@@ -7,7 +7,15 @@
 #ifndef __RCN_C_COMMON_H__
 #define __RCN_C_COMMON_H__
 
+#include <stddef.h>
+
 #define NR_ELEM(a) (sizeof(a) / sizeof(a[0]))
+
+#define CONTAINER_OF(ptr, type, member)                    \
+    ({                                                     \
+        const typeof(((type *)0)->member) *__mptr = (ptr); \
+        (type *)((char *)__mptr - offsetof(type, member)); \
+    })
 
 #ifdef __cplusplus
 namespace rcn_c
