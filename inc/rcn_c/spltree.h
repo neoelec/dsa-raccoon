b9e5bb06189870573a03275876aa0c280e1571d4
YOUNGJIN JOO <neoelec@gmail.com>
2025-09-07 18:00:29 +0900

    rcn_c/spltree: implementation
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/spltree.h
@@ -0,0 +1,375 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ *
+ * Reference : https://cubelover.tistory.com/10
+ */
+
+/* Splay Tree */
+#ifndef __RCN_C_SPLTREE_H__
+#define __RCN_C_SPLTREE_H__
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+struct splnode {
+    struct splnode *parent_;
+    struct splnode *left_;
+    struct splnode *right_;
+    size_t count_;
+    void *entry_;
+};
+
+struct spltree {
+    struct splnode *root_;
+    int (*compar_)(const void *ke, const void *in_tree);
+};
+
+static inline void spltree_clear(struct spltree *self)
+{
+    self->root_ = NULL;
+}
+
+static inline void spltree_init(struct spltree *self,
+                                int (*compar)(const void *ke,
+                                              const void *in_tree))
+{
+    spltree_clear(self);
+    self->compar_ = compar;
+}
+
+static inline bool spltree_empty(const struct spltree *self)
+{
+    return self->root_ == NULL;
+}
+
+static inline struct splnode *spltree_begin(const struct spltree *self)
+{
+    struct splnode *n = self->root_;
+
+    if (n == NULL) {
+        return NULL;
+    }
+
+    while (n->left_ != NULL) {
+        n = n->left_;
+    }
+
+    return n;
+}
+
+static inline struct splnode *spltree_rbegin(const struct spltree *self)
+{
+    struct splnode *n = self->root_;
+
+    if (n == NULL) {
+        return NULL;
+    }
+
+    while (n->right_ != NULL) {
+        n = n->right_;
+    }
+
+    return n;
+}
+
+static inline const struct splnode *spltree_end(const struct spltree *self)
+{
+    return NULL;
+}
+
+static inline const struct splnode *spltree_rend(const struct spltree *self)
+{
+    return NULL;
+}
+
+static inline struct splnode *spltree_next(const struct spltree *self,
+                                           const struct splnode *x)
+{
+    struct splnode *parent;
+
+    if (spltree_empty(self)) {
+        return NULL;
+    }
+
+    if (x->right_ != NULL) {
+        x = x->right_;
+
+        while (x->left_ != NULL) {
+            x = x->left_;
+        }
+
+        return (struct splnode *)x;
+    }
+
+    while (((parent = x->parent_) != NULL) && x == parent->right_) {
+        x = parent;
+    }
+
+    return parent;
+}
+
+static inline struct splnode *spltree_prev(const struct spltree *self,
+                                           const struct splnode *x)
+{
+    struct splnode *parent;
+
+    if (spltree_empty(self)) {
+        return NULL;
+    }
+
+    if (x->left_ != NULL) {
+        x = x->left_;
+
+        while (x->right_ != NULL) {
+            x = x->right_;
+        }
+
+        return (struct splnode *)x;
+    }
+
+    while (((parent = x->parent_) != NULL) && x == parent->left_) {
+        x = parent;
+    }
+
+    return parent;
+}
+
+static inline size_t spltree_count(const struct spltree *self)
+{
+    return spltree_empty(self) ? 0 : self->root_->count_;
+}
+
+static inline void *spltree_front(const struct spltree *self)
+{
+    return spltree_empty(self) ? NULL : spltree_begin(self)->entry_;
+}
+
+static inline void *spltree_back(const struct spltree *self)
+{
+    return spltree_empty(self) ? NULL : spltree_rbegin(self)->entry_;
+}
+
+static inline void __splnode_update(struct splnode *x)
+{
+    x->count_ = 1;
+
+    if (x->left_ != NULL) {
+        x->count_ += x->left_->count_;
+    }
+
+    if (x->right_ != NULL) {
+        x->count_ += x->right_->count_;
+    }
+}
+
+static inline void __spltree_rotate(struct spltree *self, struct splnode *x)
+{
+    struct splnode *p = x->parent_;
+    struct splnode *b;
+
+    if (x == p->left_) {
+        p->left_ = b = x->right_;
+        x->right_ = p;
+    } else {
+        p->right_ = b = x->left_;
+        x->left_ = p;
+    }
+
+    x->parent_ = p->parent_;
+    p->parent_ = x;
+
+    if (b != NULL) {
+        b->parent_ = p;
+    }
+
+    if (x->parent_ != NULL) {
+        if (p == x->parent_->left_) {
+            x->parent_->left_ = x;
+        } else {
+            x->parent_->right_ = x;
+        }
+    } else {
+        self->root_ = x;
+    }
+
+    __splnode_update(p);
+    __splnode_update(x);
+}
+
+static inline void __spltree_splay(struct spltree *self, struct splnode *x)
+{
+    while (x->parent_ != NULL) {
+        struct splnode *p = x->parent_;
+        struct splnode *g = p->parent_;
+
+        if (g != NULL) {
+            __spltree_rotate(self, (x == p->left_) == (p == g->left_) ? p : x);
+        }
+
+        __spltree_rotate(self, x);
+    }
+}
+
+static inline void spltree_insert(struct spltree *self, struct splnode *z,
+                                  void *e)
+{
+    struct splnode *p = self->root_, **pp;
+
+    z->entry_ = e;
+    z->left_ = z->right_ = NULL;
+
+    if (p == NULL) {
+        self->root_ = z;
+        z->parent_ = NULL;
+        return;
+    }
+
+    while (true) {
+        int diff = self->compar_(e, p->entry_);
+
+        if (diff == 0) {
+            return;
+        } else if (diff < 0) {
+            if (p->left_ == NULL) {
+                pp = &p->left_;
+                break;
+            }
+
+            p = p->left_;
+        } else {
+            if (p->right_ == NULL) {
+                pp = &p->right_;
+                break;
+            }
+
+            p = p->right_;
+        }
+    }
+
+    *pp = z;
+    z->parent_ = p;
+    __spltree_splay(self, z);
+}
+
+static inline struct splnode *spltree_find(const struct spltree *self,
+                                           const void *ke)
+{
+    const struct splnode *x = self->root_;
+
+    while (x != NULL) {
+        int diff = self->compar_(ke, x->entry_);
+
+        if (diff == 0) {
+            __spltree_splay((struct spltree *)self, (struct splnode *)x);
+            return (struct splnode *)x;
+        } else if (diff < 0) {
+            x = x->left_;
+        } else {
+            x = x->right_;
+        }
+    }
+
+    return NULL;
+}
+
+static inline void *spltree_erase(struct spltree *self, struct splnode *z)
+{
+    struct splnode *p;
+    struct splnode *x = spltree_find(self, z->entry_);
+    void *e;
+
+    if (x == NULL) {
+        return NULL;
+    }
+
+    e = x->entry_;
+    p = self->root_;
+
+    if (p->left_ != NULL) {
+        if (p->right_ != NULL) {
+            self->root_ = p->left_;
+            self->root_->parent_ = NULL;
+            x = self->root_;
+
+            while (x->right_ != NULL) {
+                x = x->right_;
+            }
+
+            x->right_ = p->right_;
+            p->right_->parent_ = x;
+            __spltree_splay(self, x);
+            return e;
+        }
+
+        self->root_ = p->left_;
+        self->root_->parent_ = NULL;
+        return e;
+    }
+
+    if (p->right_ != NULL) {
+        self->root_ = p->right_;
+        self->root_->parent_ = NULL;
+        return e;
+    }
+
+    self->root_ = NULL;
+
+    return e;
+}
+
+static inline void *spltree_pop_front(struct spltree *self)
+{
+    struct splnode *x = spltree_begin(self);
+
+    return x == NULL ? NULL : spltree_erase(self, x);
+}
+
+static inline void *spltree_pop_back(struct spltree *self)
+{
+    struct splnode *x = spltree_rbegin(self);
+
+    return x == NULL ? NULL : spltree_erase(self, x);
+}
+
+static inline void *spltree_at(struct spltree *self, size_t n)
+{
+    if (spltree_empty(self) || (n >= spltree_count(self))) {
+        return NULL;
+    }
+
+    struct splnode *x = self->root_;
+
+    while (true) {
+        while (x->left_ != NULL && x->left_->count_ > n) {
+            x = x->left_;
+        }
+
+        if (x->left_ != NULL) {
+            n -= x->left_->count_;
+        }
+
+        if (n == 0) {
+            break;
+        }
+
+        n--;
+        x = x->right_;
+    }
+
+    __spltree_splay(self, x);
+
+    return x->entry_;
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_SPLTREE_H__ */
---
+++ test/c_spltree_00/main.cpp
@@ -0,0 +1,236 @@
+#include "gtest/gtest.h"
+#include "rcn_c/spltree.h"
+
+struct TestData {
+    TestData(int value)
+        : value_(value)
+    {
+    }
+
+    rcn_c::splnode node_;
+    int value_;
+};
+
+class SplayTreeTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        tree_ = new rcn_c::spltree;
+        spltree_init(tree_, _ValueCompare);
+    }
+
+    void TearDown() override
+    {
+        while (!spltree_empty(tree_)) {
+            delete (TestData *)spltree_pop_front(tree_);
+        }
+
+        delete tree_;
+    }
+
+    static int _ValueCompare(const void *_ke, const void *_in_tree)
+    {
+        auto ke = (const TestData *)_ke;
+        auto in_tree = (const TestData *)_in_tree;
+
+        if (ke->value_ < in_tree->value_) {
+            return -1;
+        }
+
+        if (ke->value_ > in_tree->value_) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    rcn_c::spltree *tree_;
+};
+
+TEST_F(SplayTreeTest, InitAndEmpty)
+{
+    ASSERT_TRUE(spltree_empty(tree_));
+}
+
+TEST_F(SplayTreeTest, InsertAndFind)
+{
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    spltree_insert(tree_, &data1->node_, data1);
+    spltree_insert(tree_, &data2->node_, data2);
+    spltree_insert(tree_, &data3->node_, data3);
+
+    ASSERT_FALSE(spltree_empty(tree_));
+
+    auto key = new TestData(100);
+    auto found = spltree_find(tree_, key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_EQ(((TestData *)found->entry_)->value_, 100);
+
+    key->value_ = 50;
+    found = spltree_find(tree_, key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_EQ(((TestData *)found->entry_)->value_, 50);
+
+    key->value_ = 150;
+    found = spltree_find(tree_, key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_EQ(((TestData *)found->entry_)->value_, 150);
+
+    key->value_ = 20;
+    found = spltree_find(tree_, key);
+    ASSERT_EQ(found, nullptr);
+
+    delete key;
+}
+
+TEST_F(SplayTreeTest, BeginNextEnd)
+{
+    ASSERT_EQ(spltree_begin(tree_), nullptr);
+
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    spltree_insert(tree_, &data1->node_, data1);
+    spltree_insert(tree_, &data2->node_, data2);
+    spltree_insert(tree_, &data3->node_, data3);
+
+    rcn_c::splnode *node = spltree_begin(tree_);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(((TestData *)node->entry_)->value_, 50);
+
+    node = spltree_next(tree_, node);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(((TestData *)node->entry_)->value_, 100);
+
+    node = spltree_next(tree_, node);
+    ASSERT_NE(node, nullptr);
+    ASSERT_EQ(((TestData *)node->entry_)->value_, 150);
+
+    node = spltree_next(tree_, node);
+    ASSERT_EQ(node, nullptr);
+}
+
+TEST_F(SplayTreeTest, Erase)
+{
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    spltree_insert(tree_, &data1->node_, data1);
+    spltree_insert(tree_, &data2->node_, data2);
+    spltree_insert(tree_, &data3->node_, data3);
+
+    auto key = new TestData(50);
+    auto found = spltree_find(tree_, key);
+    ASSERT_NE(found, nullptr);
+
+    rcn_c::splnode *node_to_erase = nullptr;
+
+    for (rcn_c::splnode *node = spltree_begin(tree_);
+         node != spltree_end(tree_); node = spltree_next(tree_, node)) {
+        if (((TestData *)node->entry_)->value_ == 50) {
+            node_to_erase = node;
+            break;
+        }
+    }
+
+    ASSERT_NE(node_to_erase, nullptr);
+
+    auto erased = (TestData *)spltree_erase(tree_, node_to_erase);
+    ASSERT_EQ(erased, data2);
+
+    found = spltree_find(tree_, key);
+    ASSERT_EQ(found, nullptr);
+
+    delete key;
+}
+
+TEST_F(SplayTreeTest, Count)
+{
+    ASSERT_EQ(spltree_count(tree_), 0);
+
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    spltree_insert(tree_, &data1->node_, data1);
+    ASSERT_EQ(spltree_count(tree_), 1);
+    spltree_insert(tree_, &data2->node_, data2);
+    ASSERT_EQ(spltree_count(tree_), 2);
+    spltree_insert(tree_, &data3->node_, data3);
+    ASSERT_EQ(spltree_count(tree_), 3);
+}
+
+TEST_F(SplayTreeTest, FrontAndBack)
+{
+    ASSERT_EQ(spltree_front(tree_), nullptr);
+    ASSERT_EQ(spltree_back(tree_), nullptr);
+
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    spltree_insert(tree_, &data1->node_, data1);
+    spltree_insert(tree_, &data2->node_, data2);
+    spltree_insert(tree_, &data3->node_, data3);
+
+    ASSERT_EQ(((TestData *)spltree_front(tree_))->value_, 50);
+    ASSERT_EQ(((TestData *)spltree_back(tree_))->value_, 150);
+}
+
+TEST_F(SplayTreeTest, PopFrontAndPopBack)
+{
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    spltree_insert(tree_, &data1->node_, data1);
+    spltree_insert(tree_, &data2->node_, data2);
+    spltree_insert(tree_, &data3->node_, data3);
+
+    auto front = (TestData *)spltree_pop_front(tree_);
+    ASSERT_EQ(front->value_, 50);
+    ASSERT_EQ(spltree_count(tree_), 2);
+
+    auto back = (TestData *)spltree_pop_back(tree_);
+    ASSERT_EQ(back->value_, 150);
+    ASSERT_EQ(spltree_count(tree_), 1);
+
+    auto remaining = (TestData *)spltree_pop_front(tree_);
+    ASSERT_EQ(remaining->value_, 100);
+    ASSERT_EQ(spltree_count(tree_), 0);
+    ASSERT_TRUE(spltree_empty(tree_));
+}
+
+TEST_F(SplayTreeTest, At)
+{
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    spltree_insert(tree_, &data1->node_, data1);
+    spltree_insert(tree_, &data2->node_, data2);
+    spltree_insert(tree_, &data3->node_, data3);
+
+    auto found = (TestData *)spltree_at(tree_, 0);
+    ASSERT_NE(found, nullptr);
+
+    found = (TestData *)spltree_at(tree_, 1);
+    ASSERT_NE(found, nullptr);
+
+    found = (TestData *)spltree_at(tree_, 2);
+    ASSERT_NE(found, nullptr);
+
+    found = (TestData *)spltree_at(tree_, 3);
+    ASSERT_EQ(found, nullptr);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
