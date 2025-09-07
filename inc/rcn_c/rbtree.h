d7ccf5b5a7fbc4cb13871ba1b3d682bdfb1f035b
YOUNGJIN JOO <neoelec@gmail.com>
2025-09-07 16:27:08 +0900

    rcn_c/rbtree: implement
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/rbtree.h
@@ -0,0 +1,481 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+/*
+ * Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Red-Black Tree */
+#ifndef __RCN_C_RBTREE_H__
+#define __RCN_C_RBTREE_H__
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+enum rbcolor {
+    RBCOLOR_RED = 0,
+    RBCOLOR_BLACK,
+};
+
+struct rbnode {
+    enum rbcolor color_;
+    struct rbnode *parent_;
+    struct rbnode *left_;
+    struct rbnode *right_;
+    void *entry_;
+};
+
+struct rbtree {
+    struct rbnode *root_;
+    int (*compar_)(const void *ke, const void *in_tree);
+    struct rbnode NIL_;
+};
+
+static inline const struct rbnode *rbtree_nil(const struct rbtree *_self)
+{
+    struct rbtree *self = (struct rbtree *)_self;
+
+    return &self->NIL_;
+}
+
+static inline void rbtree_clear(struct rbtree *self)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+
+    self->root_ = (struct rbnode *)NIL;
+
+    self->NIL_.color_ = RBCOLOR_BLACK;
+    self->NIL_.left_ = (struct rbnode *)NIL;
+    self->NIL_.right_ = (struct rbnode *)NIL;
+    self->NIL_.entry_ = NULL;
+}
+
+static inline void rbtree_init(struct rbtree *self,
+                               int (*compar)(const void *ke,
+                                             const void *in_tree))
+{
+    rbtree_clear(self);
+    self->compar_ = compar;
+}
+
+static inline bool rbtree_empty(const struct rbtree *self)
+{
+    return self->root_ == rbtree_nil(self);
+}
+
+static inline struct rbnode *rbtree_begin(const struct rbtree *self)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *n = self->root_;
+
+    if (n == NIL) {
+        return (struct rbnode *)NIL;
+    }
+
+    while (n->left_ != NIL) {
+        n = n->left_;
+    }
+
+    return n;
+}
+
+static inline struct rbnode *rbtree_rbegin(const struct rbtree *self)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *n = self->root_;
+
+    if (n == NIL) {
+        return (struct rbnode *)NIL;
+    }
+
+    while (n->right_ != NIL) {
+        n = n->right_;
+    }
+
+    return n;
+}
+
+static inline const struct rbnode *rbtree_end(const struct rbtree *self)
+{
+    return rbtree_nil(self);
+}
+
+static inline const struct rbnode *rbtree_rend(const struct rbtree *self)
+{
+    return rbtree_nil(self);
+}
+
+static inline struct rbnode *rbtree_next(const struct rbtree *self,
+                                         const struct rbnode *x)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *parent;
+
+    if (rbtree_empty(self)) {
+        return (struct rbnode *)NIL;
+    }
+
+    if (x->right_ != NIL) {
+        x = x->right_;
+
+        while (x->left_ != NIL) {
+            x = x->left_;
+        }
+
+        return (struct rbnode *)x;
+    }
+
+    while (((parent = x->parent_) != NIL) && x == parent->right_) {
+        x = parent;
+    }
+
+    return parent;
+}
+
+static inline struct rbnode *rbtree_prev(const struct rbtree *self,
+                                         const struct rbnode *x)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *parent;
+
+    if (rbtree_empty(self)) {
+        return (struct rbnode *)NIL;
+    }
+
+    if (x->left_ != NIL) {
+        x = x->left_;
+
+        while (x->right_ != NIL) {
+            x = x->right_;
+        }
+
+        return (struct rbnode *)x;
+    }
+
+    while (((parent = x->parent_) != NIL) && x == parent->left_) {
+        x = parent;
+    }
+
+    return parent;
+}
+
+static inline size_t rbtree_count(const struct rbtree *self)
+{
+    size_t i = 0;
+
+    for (const struct rbnode *x = rbtree_begin(self); x != rbtree_end(self);
+         x = rbtree_next(self, x)) {
+        i++;
+    }
+
+    return i;
+}
+
+static inline void *rbtree_front(const struct rbtree *self)
+{
+    return rbtree_empty(self) ? NULL : rbtree_begin(self)->entry_;
+}
+
+static inline void *rbtree_back(const struct rbtree *self)
+{
+    return rbtree_empty(self) ? NULL : rbtree_rbegin(self)->entry_;
+}
+
+static inline void __rbtree_left_rotate(struct rbtree *self, struct rbnode *x)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *y = x->right_;
+
+    x->right_ = y->left_;
+
+    if (y->left_ != NIL) {
+        y->left_->parent_ = x;
+    }
+
+    y->parent_ = x->parent_;
+
+    if (x->parent_ == NIL) {
+        self->root_ = y;
+    } else if (x == x->parent_->left_) {
+        x->parent_->left_ = y;
+    } else {
+        x->parent_->right_ = y;
+    }
+
+    y->left_ = x;
+    x->parent_ = y;
+}
+
+static inline void __rbtree_right_rotate(struct rbtree *self, struct rbnode *x)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *y = x->left_;
+
+    x->left_ = y->right_;
+
+    if (y->right_ != NIL) {
+        y->right_->parent_ = x;
+    }
+
+    y->parent_ = x->parent_;
+
+    if (x->parent_ == NIL) {
+        self->root_ = y;
+    } else if (x == x->parent_->left_) {
+        x->parent_->left_ = y;
+    } else {
+        x->parent_->right_ = y;
+    }
+
+    y->right_ = x;
+    x->parent_ = y;
+}
+
+static inline void __rbtree_insert_fixup(struct rbtree *self, struct rbnode *z)
+{
+    while (z->parent_->color_ == RBCOLOR_RED) {
+        if (z->parent_ == z->parent_->parent_->left_) {
+            struct rbnode *y = z->parent_->parent_->right_;
+
+            if (y->color_ == RBCOLOR_RED) {
+                z->parent_->color_ = RBCOLOR_BLACK;
+                y->color_ = RBCOLOR_BLACK;
+                z->parent_->parent_->color_ = RBCOLOR_RED;
+                z = z->parent_->parent_;
+            } else {
+                if (z == z->parent_->right_) {
+                    z = z->parent_;
+                    __rbtree_left_rotate(self, z);
+                }
+
+                z->parent_->color_ = RBCOLOR_BLACK;
+                z->parent_->parent_->color_ = RBCOLOR_RED;
+                __rbtree_right_rotate(self, z->parent_->parent_);
+            }
+        } else {
+            struct rbnode *y = z->parent_->parent_->left_;
+
+            if (y->color_ == RBCOLOR_RED) {
+                z->parent_->color_ = RBCOLOR_BLACK;
+                y->color_ = RBCOLOR_BLACK;
+                z->parent_->parent_->color_ = RBCOLOR_RED;
+                z = z->parent_->parent_;
+            } else {
+                if (z == z->parent_->left_) {
+                    z = z->parent_;
+                    __rbtree_right_rotate(self, z);
+                }
+
+                z->parent_->color_ = RBCOLOR_BLACK;
+                z->parent_->parent_->color_ = RBCOLOR_RED;
+                __rbtree_left_rotate(self, z->parent_->parent_);
+            }
+        }
+    }
+
+    self->root_->color_ = RBCOLOR_BLACK;
+}
+
+static inline void rbtree_insert(struct rbtree *self, struct rbnode *z, void *e)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *x = self->root_;
+    struct rbnode *y = (struct rbnode *)NIL;
+    int diff = 0;
+
+    while (x != NIL) {
+        y = x;
+        diff = self->compar_(e, y->entry_);
+
+        if (diff < 0) {
+            x = x->left_;
+        } else {
+            x = x->right_;
+        }
+    }
+
+    z->entry_ = e;
+    z->parent_ = y;
+    z->left_ = z->right_ = (struct rbnode *)NIL;
+
+    if (y == NIL) {
+        self->root_ = z;
+    } else if (diff < 0) {
+        y->left_ = z;
+    } else {
+        y->right_ = z;
+    }
+
+    __rbtree_insert_fixup(self, z);
+}
+
+static inline void __rbtree_transplant(struct rbtree *self, struct rbnode *u,
+                                       struct rbnode *v)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+
+    if (u->parent_ == NIL) {
+        self->root_ = v;
+    } else if (u == u->parent_->left_) {
+        u->parent_->left_ = v;
+    } else {
+        u->parent_->right_ = v;
+    }
+
+    v->parent_ = u->parent_;
+}
+
+static inline void __rbtree_delete_fixup(struct rbtree *self, struct rbnode *x)
+{
+    while (x != self->root_ && x->color_ == RBCOLOR_BLACK) {
+        if (x == x->parent_->left_) {
+            struct rbnode *w = x->parent_->right_;
+
+            if (w->color_ == RBCOLOR_RED) {
+                w->color_ = RBCOLOR_BLACK;
+                x->parent_->color_ = RBCOLOR_RED;
+                __rbtree_left_rotate(self, x->parent_);
+                w = x->parent_->right_;
+            }
+
+            if ((w->left_->color_ == RBCOLOR_BLACK) &&
+                (w->right_->color_ == RBCOLOR_BLACK)) {
+                w->color_ = RBCOLOR_RED;
+                x = x->parent_;
+            } else {
+                if (w->right_->color_ == RBCOLOR_BLACK) {
+                    w->left_->color_ = RBCOLOR_BLACK;
+                    w->color_ = RBCOLOR_RED;
+                    __rbtree_right_rotate(self, w);
+                    w = x->parent_->right_;
+                }
+
+                w->color_ = x->parent_->color_;
+                x->parent_->color_ = RBCOLOR_BLACK;
+                w->right_->color_ = RBCOLOR_BLACK;
+                __rbtree_left_rotate(self, x->parent_);
+                x = self->root_;
+            }
+        } else {
+            struct rbnode *w = x->parent_->left_;
+
+            if (w->color_ == RBCOLOR_RED) {
+                w->color_ = RBCOLOR_BLACK;
+                x->parent_->color_ = RBCOLOR_RED;
+                __rbtree_right_rotate(self, x->parent_);
+                w = x->parent_->left_;
+            }
+
+            if ((w->right_->color_ == RBCOLOR_BLACK) &&
+                (w->left_->color_ == RBCOLOR_BLACK)) {
+                w->color_ = RBCOLOR_RED;
+                x = x->parent_;
+            } else {
+                if (w->left_->color_ == RBCOLOR_BLACK) {
+                    w->right_->color_ = RBCOLOR_BLACK;
+                    w->color_ = RBCOLOR_RED;
+                    __rbtree_left_rotate(self, w);
+                    w = x->parent_->left_;
+                }
+
+                w->color_ = x->parent_->color_;
+                x->parent_->color_ = RBCOLOR_BLACK;
+                w->left_->color_ = RBCOLOR_BLACK;
+                __rbtree_right_rotate(self, x->parent_);
+                x = self->root_;
+            }
+        }
+    }
+
+    x->color_ = RBCOLOR_BLACK;
+}
+
+static inline struct rbnode *rbtree_find(const struct rbtree *self,
+                                         const void *ke)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *x = self->root_;
+
+    while (x != NIL) {
+        int diff;
+
+        diff = self->compar_(ke, x->entry_);
+
+        if (diff == 0) {
+            return x;
+        } else if (diff < 0) {
+            x = x->left_;
+        } else {
+            x = x->right_;
+        }
+    }
+
+    return (struct rbnode *)NIL;
+}
+
+static inline void *rbtree_erase(struct rbtree *self, struct rbnode *z)
+{
+    const struct rbnode *const NIL = rbtree_nil(self);
+    struct rbnode *x = rbtree_find(self, z->entry_);
+    struct rbnode *y;
+    enum rbcolor y_original_color;
+    void *e;
+
+    if (x == NIL) {
+        return NULL;
+    }
+
+    e = z->entry_;
+    y = z;
+    y_original_color = y->color_;
+
+    if (z->left_ == NIL) {
+        x = z->right_;
+        __rbtree_transplant(self, z, z->right_);
+    } else if (z->right_ == NIL) {
+        x = z->left_;
+        __rbtree_transplant(self, z, z->left_);
+    } else {
+        y_original_color = y->color_;
+        x = y->right_;
+
+        if (y != z->right_) {
+            __rbtree_transplant(self, y, y->right_);
+            y->right_ = z->right_;
+            y->right_->parent_ = y;
+        } else {
+            x->parent_ = y;
+        }
+
+        __rbtree_transplant(self, z, y);
+        y->left_ = z->left_;
+        y->left_->parent_ = y;
+        y->color_ = z->color_;
+    }
+
+    if (y_original_color == RBCOLOR_BLACK) {
+        __rbtree_delete_fixup(self, x);
+    }
+
+    return e;
+}
+
+static inline void *rbtree_pop_front(struct rbtree *self)
+{
+    return rbtree_erase(self, rbtree_begin(self));
+}
+
+static inline void *rbtree_pop_back(struct rbtree *self)
+{
+    return rbtree_erase(self, rbtree_rbegin(self));
+}
+
+#ifdef __cplusplus
+}
+#endif /* namespace rcn_c */
+
+#endif /* __RCN_C_RBTREE_H__ */
---
+++ test/c_rbtree_00/main.cpp
@@ -0,0 +1,220 @@
+#include "gtest/gtest.h"
+#include "rcn_c/rbtree.h"
+
+struct TestData {
+    TestData(int value)
+        : value_(value)
+    {
+    }
+
+    rcn_c::rbnode node_;
+    int value_;
+};
+
+class RedBlackTreeTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        tree_ = new rcn_c::rbtree;
+        rbtree_init(tree_, _ValueCompare);
+    }
+
+    void TearDown() override
+    {
+        while (!rbtree_empty(tree_)) {
+            delete (TestData *)rbtree_pop_front(tree_);
+        }
+
+        delete tree_;
+    }
+
+    static int _ValueCompare(const void *_ke, const void *_in_tree)
+    {
+        auto ke = (const TestData *)_ke;
+        auto in_tree = (const TestData *)_in_tree;
+
+        if (ke->value_ < in_tree->value_) {
+            return -1;
+        }
+
+        if (ke->value_ > in_tree->value_) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    rcn_c::rbtree *tree_;
+};
+
+TEST_F(RedBlackTreeTest, InitAndEmpty)
+{
+    ASSERT_TRUE(rbtree_empty(tree_));
+}
+
+TEST_F(RedBlackTreeTest, InsertAndFind)
+{
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    rbtree_insert(tree_, &data1->node_, data1);
+    rbtree_insert(tree_, &data2->node_, data2);
+    rbtree_insert(tree_, &data3->node_, data3);
+
+    ASSERT_FALSE(rbtree_empty(tree_));
+
+    auto key = new TestData(100);
+    auto found = rbtree_find(tree_, key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_NE(found, rbtree_nil(tree_));
+    ASSERT_EQ(((TestData *)found->entry_)->value_, 100);
+
+    key->value_ = 50;
+    found = rbtree_find(tree_, key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_NE(found, rbtree_nil(tree_));
+    ASSERT_EQ(((TestData *)found->entry_)->value_, 50);
+
+    key->value_ = 150;
+    found = rbtree_find(tree_, key);
+    ASSERT_NE(found, nullptr);
+    ASSERT_NE(found, rbtree_nil(tree_));
+    ASSERT_EQ(((TestData *)found->entry_)->value_, 150);
+
+    key->value_ = 20;
+    found = rbtree_find(tree_, key);
+    ASSERT_EQ(found, rbtree_nil(tree_));
+
+    delete key;
+}
+
+TEST_F(RedBlackTreeTest, BeginNextEnd)
+{
+    ASSERT_EQ(rbtree_begin(tree_), rbtree_nil(tree_));
+
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    rbtree_insert(tree_, &data1->node_, data1);
+    rbtree_insert(tree_, &data2->node_, data2);
+    rbtree_insert(tree_, &data3->node_, data3);
+
+    rcn_c::rbnode *node = rbtree_begin(tree_);
+    ASSERT_NE(node, nullptr);
+    ASSERT_NE(node, rbtree_nil(tree_));
+    ASSERT_EQ(((TestData *)node->entry_)->value_, 50);
+
+    node = rbtree_next(tree_, node);
+    ASSERT_NE(node, nullptr);
+    ASSERT_NE(node, rbtree_nil(tree_));
+    ASSERT_EQ(((TestData *)node->entry_)->value_, 100);
+
+    node = rbtree_next(tree_, node);
+    ASSERT_NE(node, nullptr);
+    ASSERT_NE(node, rbtree_nil(tree_));
+    ASSERT_EQ(((TestData *)node->entry_)->value_, 150);
+
+    node = rbtree_next(tree_, node);
+    ASSERT_EQ(node, rbtree_nil(tree_));
+}
+
+TEST_F(RedBlackTreeTest, Erase)
+{
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    rbtree_insert(tree_, &data1->node_, data1);
+    rbtree_insert(tree_, &data2->node_, data2);
+    rbtree_insert(tree_, &data3->node_, data3);
+
+    auto key = new TestData(50);
+    auto found = rbtree_find(tree_, key);
+    ASSERT_NE(found, rbtree_nil(tree_));
+
+    rcn_c::rbnode *node_to_erase = (rcn_c::rbnode *)rbtree_nil(tree_);
+
+    for (rcn_c::rbnode *node = rbtree_begin(tree_); node != rbtree_end(tree_);
+         node = rbtree_next(tree_, node)) {
+        if (((TestData *)node->entry_)->value_ == 50) {
+            node_to_erase = node;
+            break;
+        }
+    }
+
+    ASSERT_NE(node_to_erase, nullptr);
+    ASSERT_NE(node_to_erase, rbtree_nil(tree_));
+
+    auto erased = (TestData *)rbtree_erase(tree_, node_to_erase);
+    ASSERT_EQ(erased, data2);
+
+    found = rbtree_find(tree_, key);
+    ASSERT_EQ(found, rbtree_nil(tree_));
+
+    delete key;
+}
+
+TEST_F(RedBlackTreeTest, Count)
+{
+    ASSERT_EQ(rbtree_count(tree_), 0);
+
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    rbtree_insert(tree_, &data1->node_, data1);
+    ASSERT_EQ(rbtree_count(tree_), 1);
+    rbtree_insert(tree_, &data2->node_, data2);
+    ASSERT_EQ(rbtree_count(tree_), 2);
+    rbtree_insert(tree_, &data3->node_, data3);
+    ASSERT_EQ(rbtree_count(tree_), 3);
+}
+
+TEST_F(RedBlackTreeTest, FrontAndBack)
+{
+    ASSERT_EQ(rbtree_front(tree_), nullptr);
+    ASSERT_EQ(rbtree_back(tree_), nullptr);
+
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    rbtree_insert(tree_, &data1->node_, data1);
+    rbtree_insert(tree_, &data2->node_, data2);
+    rbtree_insert(tree_, &data3->node_, data3);
+
+    ASSERT_EQ(((TestData *)rbtree_front(tree_))->value_, 50);
+    ASSERT_EQ(((TestData *)rbtree_back(tree_))->value_, 150);
+}
+
+TEST_F(RedBlackTreeTest, PopFrontAndPopBack)
+{
+    auto data1 = new TestData(100);
+    auto data2 = new TestData(50);
+    auto data3 = new TestData(150);
+
+    rbtree_insert(tree_, &data1->node_, data1);
+    rbtree_insert(tree_, &data2->node_, data2);
+    rbtree_insert(tree_, &data3->node_, data3);
+
+    auto front = (TestData *)rbtree_pop_front(tree_);
+    ASSERT_EQ(front->value_, 50);
+    ASSERT_EQ(rbtree_count(tree_), 2);
+
+    auto back = (TestData *)rbtree_pop_back(tree_);
+    ASSERT_EQ(back->value_, 150);
+    ASSERT_EQ(rbtree_count(tree_), 1);
+
+    auto remaining = (TestData *)rbtree_pop_front(tree_);
+    ASSERT_EQ(remaining->value_, 100);
+    ASSERT_EQ(rbtree_count(tree_), 0);
+    ASSERT_TRUE(rbtree_empty(tree_));
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
