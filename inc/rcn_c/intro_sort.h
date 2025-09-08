384355fcee5b172d462f5523e92853220342bc27
YOUNGJIN JOO <neoelec@gmail.com>
2025-09-08 09:54:20 +0000

    rcn_c:intro_sort: implementation
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/intro_sort.h
@@ -0,0 +1,57 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+/*
+ * Copyright (c) 2024-2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Intro Sort */
+#ifndef __RCN_C_INTRO_SORT_H__
+#define __RCN_C_INTRO_SORT_H__
+
+#include "heap_sort.h"
+#include "ilog2.h"
+#include "insertion_sort.h"
+#include "quick_sort.h"
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+static void __intro_sort(void *base, ssize_t left, ssize_t right, size_t size,
+                         ssize_t depth_limit,
+                         int (*compar)(const void *a, const void *b))
+{
+#define __base(n) (&((char *)base)[(n) * size])
+
+    if (right - left > 16) {
+        if (depth_limit == 0) {
+            heap_sort(__base(left), right - left + 1, size, compar);
+            return;
+        }
+
+        depth_limit--;
+
+        ssize_t j = __partition(base, left, right, size, compar);
+        __intro_sort(base, left, j - 1, size, depth_limit, compar);
+        __intro_sort(base, j + 1, right, size, depth_limit, compar);
+    } else {
+        insertion_sort(__base(left), right - left + 1, size, compar);
+    }
+
+#undef __base
+}
+
+static inline void intro_sort(void *base, size_t nmemb, size_t size,
+                              int (*compar)(const void *a, const void *))
+{
+    ssize_t depth_limit = 2 * ilog2l((unsigned long)nmemb);
+
+    __intro_sort(base, 0, nmemb - 1, size, depth_limit, compar);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RCN_C_INTRO_SORT_H__ */
---
+++ test/c_intro_sort_00/main.cpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <cstring>
+
+#include "gtest/gtest.h"
+#include "rcn_c/common.h"
+#include "rcn_c/intro_sort.h"
+
+int IntCompar(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+TEST(IntroSortTest, EmptyArray)
+{
+    int arr[0];
+
+    rcn_c::intro_sort(arr, NR_ELEM(arr), sizeof(arr[0]), IntCompar);
+    ASSERT_EQ(NR_ELEM(arr), 0);
+}
+
+TEST(IntroSortTest, SingleElementArray)
+{
+    int arr[] = { 5 };
+
+    rcn_c::intro_sort(arr, NR_ELEM(arr), sizeof(arr[0]), IntCompar);
+    ASSERT_EQ(NR_ELEM(arr), 1);
+    ASSERT_EQ(arr[0], 5);
+}
+
+TEST(IntroSortTest, AlreadySortedArray)
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int expected[NR_ELEM(arr)];
+
+    std::memcpy(expected, arr, sizeof(arr));
+    rcn_c::intro_sort(arr, NR_ELEM(arr), sizeof(arr[0]), IntCompar);
+    EXPECT_TRUE(0 == std::memcmp(arr, expected, sizeof(expected)));
+}
+
+TEST(IntroSortTest, ReverseSortedArray)
+{
+    int arr[] = { 5, 4, 3, 2, 1 };
+    int expected[] = { 1, 2, 3, 4, 5 };
+
+    rcn_c::intro_sort(arr, NR_ELEM(arr), sizeof(arr[0]), IntCompar);
+    EXPECT_TRUE(0 == std::memcmp(arr, expected, sizeof(expected)));
+}
+
+TEST(IntroSortTest, RandomArray)
+{
+    int arr[] = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+    int expected[NR_ELEM(arr)];
+
+    std::memcpy(expected, arr, sizeof(arr));
+    std::sort(expected, expected + NR_ELEM(expected));
+    rcn_c::intro_sort(arr, NR_ELEM(arr), sizeof(arr[0]), IntCompar);
+    EXPECT_TRUE(0 == std::memcmp(arr, expected, sizeof(expected)));
+}
+
+TEST(IntroSortTest, DuplicateElements)
+{
+    int arr[] = { 5, 2, 1, 2, 5, 3, 1 };
+    int expected[NR_ELEM(arr)];
+
+    std::memcpy(expected, arr, sizeof(arr));
+    std::sort(expected, expected + NR_ELEM(expected));
+    rcn_c::intro_sort(arr, NR_ELEM(arr), sizeof(arr[0]), IntCompar);
+    EXPECT_TRUE(0 == std::memcmp(arr, expected, sizeof(expected)));
+}
+
+TEST(IntroSortTest, LargeArray)
+{
+    int arr[1000];
+
+    for (size_t i = 0; i < NR_ELEM(arr); ++i) {
+        arr[i] = rand() % 100;
+    }
+
+    int expected[NR_ELEM(arr)];
+    std::memcpy(expected, arr, sizeof(arr));
+    std::sort(expected, expected + NR_ELEM(expected));
+    rcn_c::intro_sort(arr, NR_ELEM(arr), sizeof(arr[0]), IntCompar);
+    EXPECT_TRUE(0 == std::memcmp(arr, expected, sizeof(expected)));
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
