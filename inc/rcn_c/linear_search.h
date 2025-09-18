7ad1e290d085eab3904ae3477433ff4b556dc9bc
YOUNGJIN JOO <neoelec@gmail.com>
2025-09-18 22:12:08 +0900

    linear_search: implementation
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ inc/rcn_c/linear_search.h
@@ -0,0 +1,93 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+/*
+ * Copyright (c) 2024-2025 YOUNGJIN JOO (neoelec@gmail.com)
+ */
+
+/* Linear Search */
+#ifndef __RCN_C_LINEAR_SEARCH_H__
+#define __RCN_C_LINEAR_SEARCH_H__
+
+#include <errno.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/types.h>
+
+#ifdef __cplusplus
+namespace rcn_c
+{
+#endif
+
+#define __base(n) (&((char *)base)[(n) * size])
+
+static inline ssize_t
+__linear_search(const void *key, const void *base, size_t nmemb, size_t size,
+                int (*compar)(const void *a, const void *b))
+{
+    size_t pos = 0;
+
+    while (pos < nmemb) {
+        if (compar(key, __base(pos)) == 0) {
+            return pos;
+        }
+
+        pos++;
+    }
+
+    return -EEXIST;
+}
+
+static inline void *linear_search(const void *key, const void *base,
+                                  size_t nmemb, size_t size,
+                                  int (*compar)(const void *a, const void *b))
+{
+    ssize_t pos = __linear_search(key, base, nmemb, size, compar);
+
+    return pos >= 0 ? __base(pos) : NULL;
+}
+
+static inline size_t linear_insert(void *key, void *base, size_t *nmemb,
+                                   size_t size,
+                                   int (*compar)(const void *a, const void *b))
+{
+    size_t pos = 0;
+
+    while (compar(key, __base(pos)) > 0 && pos < *nmemb) {
+        pos++;
+    }
+
+    for (size_t i = *nmemb; i > pos; --i) {
+        memcpy(__base(i), __base(i - 1), size);
+    }
+
+    memcpy(__base(pos), key, size);
+    (*nmemb)++;
+
+    return pos;
+}
+
+static inline ssize_t linear_delete(void *key, void *base, size_t *nmemb,
+                                    size_t size,
+                                    int (*compar)(const void *a, const void *b))
+{
+    ssize_t pos;
+
+    if (*nmemb <= 0) {
+        return -ERANGE;
+    } else if ((pos = __linear_search(key, base, *nmemb, size, compar)) < 0) {
+        return -EEXIST;
+    }
+
+    memcpy(__base(pos), __base(pos + 1), (*nmemb - pos) * size);
+    (*nmemb)--;
+
+    return pos;
+}
+
+#undef __base
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RCN_C_LINEAR_SEARCH_H__ */
---
+++ test/c_linear_search_00/main.cpp
@@ -0,0 +1,140 @@
+#include "gtest/gtest.h"
+#include "rcn_c/common.h"
+#include "rcn_c/linear_search.h"
+
+int IntCompar(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+TEST(LinearSearchTest, EmptyArray)
+{
+    int key = 5;
+    int arr[] = {};
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+
+    ASSERT_EQ(nullptr, result);
+}
+
+TEST(LinearSearchTest, SingleElementFound)
+{
+    int key = 5;
+    int arr[] = { 5 };
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+
+    ASSERT_NE(nullptr, result);
+    ASSERT_EQ(key, *(int *)result);
+}
+
+TEST(LinearSearchTest, SingleElementNotFound)
+{
+    int key = 5;
+    int arr[] = { 10 };
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+
+    ASSERT_EQ(nullptr, result);
+}
+
+TEST(LinearSearchTest, MultipleElementsFound)
+{
+    int key = 5;
+    int arr[] = { 1, 3, 5, 7, 9 };
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+
+    ASSERT_NE(nullptr, result);
+    ASSERT_EQ(key, *(int *)result);
+}
+
+TEST(LinearSearchTest, MultipleElementsNotFound)
+{
+    int key = 6;
+    int arr[] = { 1, 3, 5, 7, 9 };
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+
+    ASSERT_EQ(nullptr, result);
+}
+
+TEST(LinearSearchTest, MultipleElementsFoundFirst)
+{
+    int key = 1;
+    int arr[] = { 1, 3, 5, 7, 9 };
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+
+    ASSERT_NE(nullptr, result);
+    ASSERT_EQ(key, *(int *)result);
+}
+
+TEST(LinearSearchTest, MultipleElementsFoundLast)
+{
+    int key = 9;
+    int arr[] = { 1, 3, 5, 7, 9 };
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+
+    ASSERT_NE(nullptr, result);
+    ASSERT_EQ(key, *(int *)result);
+}
+
+TEST(LinearSearchTest, LargeArrayFound)
+{
+    int key = 500;
+    int arr[1000];
+
+    for (size_t i = 0; i < NR_ELEM(arr); ++i) {
+        arr[i] = i;
+    }
+
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+    ASSERT_NE(nullptr, result);
+    ASSERT_EQ(key, *(int *)result);
+}
+
+TEST(LinearSearchTest, LargeArrayNotFound)
+{
+    int key = 1000;
+    int arr[1000];
+
+    for (size_t i = 0; i < NR_ELEM(arr); ++i) {
+        arr[i] = i;
+    }
+
+    void *result = rcn_c::linear_search(&key, arr, NR_ELEM(arr), sizeof(arr[0]),
+                                        IntCompar);
+    ASSERT_EQ(nullptr, result);
+}
+
+TEST(LinearSearchTest, LinearInsertAndDelete)
+{
+    int key = 4;
+    int arr[6] = { 1, 3, 5, 7, 9 };
+    size_t nmemb = 5;
+    ssize_t pos;
+
+    pos = rcn_c::linear_insert(&key, arr, &nmemb, sizeof(arr[0]), IntCompar);
+    ASSERT_EQ(2, pos);
+    ASSERT_EQ(key, arr[pos]);
+    ASSERT_EQ(5, arr[pos + 1]);
+    ASSERT_EQ(7, arr[pos + 2]);
+    ASSERT_EQ(9, arr[pos + 3]);
+    ASSERT_EQ(6, nmemb);
+
+    pos = rcn_c::linear_delete(&key, arr, &nmemb, sizeof(arr[0]), IntCompar);
+    ASSERT_EQ(2, pos);
+    ASSERT_EQ(5, arr[pos]);
+    ASSERT_EQ(7, arr[pos + 1]);
+    ASSERT_EQ(9, arr[pos + 2]);
+    ASSERT_EQ(5, nmemb);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
